59eaeac10bd297f95355646bc71d7602674b873f
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-05-29 20:08:54 +0100

    Forgot to add the generic method argument for SessionFactory

--- common/src/main/java/com/nukkitx/network/SessionFactory.java
+++ common/src/main/java/com/nukkitx/network/SessionFactory.java
@@ -2,5 +2,5 @@ package com.nukkitx.network;
 
 public interface SessionFactory<T extends NetworkSession, U extends NetworkPacket> {
 
-    T createSession(SessionConnection connection);
+    T createSession(SessionConnection<U> connection);
 }
