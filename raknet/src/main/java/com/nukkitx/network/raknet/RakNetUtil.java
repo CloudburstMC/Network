d67e73e669ac09873eabe4bedb02fb74dd1e6a23
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-06-16 14:20:37 +0100

    Bump RakNet version to comply with new Bedrock layer protocol changes

--- raknet/src/main/java/com/nukkitx/network/raknet/RakNetUtil.java
+++ raknet/src/main/java/com/nukkitx/network/raknet/RakNetUtil.java
@@ -14,7 +14,7 @@ public class RakNetUtil {
     public static final byte[] RAKNET_UNCONNECTED_MAGIC = new byte[]{
             0, -1, -1, 0, -2, -2, -2, -2, -3, -3, -3, -3, 18, 52, 86, 120
     };
-    public static final byte RAKNET_PROTOCOL_VERSION = 8; // Mojangs version.
+    public static final byte RAKNET_PROTOCOL_VERSION = 9; // Mojangs version.
     public static final short MINIMUM_MTU_SIZE = 400;
     public static final short MAXIMUM_MTU_SIZE = 1492;
     public static final int MAX_ENCAPSULATED_HEADER_SIZE = 9;
