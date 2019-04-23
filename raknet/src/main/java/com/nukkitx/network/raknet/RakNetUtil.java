f8e4dca7d94eb2ecf98d01f29c7fea5be4b6125b
SupremeMortal <suprememortal@users.noreply.github.com>
2019-04-23 16:50:05 +0100

    Correct minimum and maximum MTU constants

--- raknet/src/main/java/com/nukkitx/network/raknet/RakNetUtil.java
+++ raknet/src/main/java/com/nukkitx/network/raknet/RakNetUtil.java
@@ -15,8 +15,8 @@ public class RakNetUtil {
             0, -1, -1, 0, -2, -2, -2, -2, -3, -3, -3, -3, 18, 52, 86, 120
     };
     public static final byte RAKNET_PROTOCOL_VERSION = 9; // Mojang's version.
-    public static final short MINIMUM_MTU_SIZE = 400;
-    public static final short MAXIMUM_MTU_SIZE = 1492;
+    public static final short MINIMUM_MTU_SIZE = 576;
+    public static final short MAXIMUM_MTU_SIZE = 1464;
     public static final int MAX_ENCAPSULATED_HEADER_SIZE = 9;
     public static final int MAX_MESSAGE_HEADER_SIZE = 23;
     public static final InetSocketAddress LOOPBACK = new InetSocketAddress(InetAddress.getLoopbackAddress(), 19132);
