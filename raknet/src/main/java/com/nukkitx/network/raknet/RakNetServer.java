c1554470d3f43a9665200ec70797e4f533b4017a
EmreTr1 <emre.yavuz.842@gmail.com>
2020-11-19 18:17:39 +0300

    RakNetServer: Fixed stuck on first connection request

--- raknet/src/main/java/com/nukkitx/network/raknet/RakNetServer.java
+++ raknet/src/main/java/com/nukkitx/network/raknet/RakNetServer.java
@@ -166,7 +166,7 @@ public class RakNetServer extends RakNet {
 
         RakNetServerSession session = this.sessionsByAddress.get(packet.sender());
 
-        if (session != null) {
+        if (session != null && session.getState() == RakNetState.CONNECTED) {
             this.sendAlreadyConnected(ctx, packet.sender());
         } else if (this.protocolVersion >= 0 && this.protocolVersion != protocolVersion) {
             this.sendIncompatibleProtocolVersion(ctx, packet.sender());
@@ -174,7 +174,7 @@ public class RakNetServer extends RakNet {
             this.sendNoFreeIncomingConnections(ctx, packet.sender());
         } else if (this.listener != null && !this.listener.onConnectionRequest(packet.sender())) {
             this.sendConnectionBanned(ctx, packet.sender());
-        } else {
+        } else if (session == null) {
             // Passed all checks. Now create the session and send the first reply.
             session = new RakNetServerSession(this, packet.sender(), ctx.channel(), mtu, protocolVersion);
             session.setState(RakNetState.INITIALIZING);
@@ -184,6 +184,8 @@ public class RakNetServer extends RakNet {
                     listener.onSessionCreation(session);
                 }
             }
+        } else {
+            session.sendOpenConnectionReply1(); // probably a packet loss occurred, send the reply again
         }
     }
 
