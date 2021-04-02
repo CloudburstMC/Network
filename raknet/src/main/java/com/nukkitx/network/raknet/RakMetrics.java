9a2901aa3b31a5b958bcaec266523220711a957e
Alemiz112 <alex@mizerak.eu>
2021-04-02 10:36:14 +0200

    Metrics

--- raknet/src/main/java/com/nukkitx/network/raknet/RakMetrics.java
+++ raknet/src/main/java/com/nukkitx/network/raknet/RakMetrics.java
@@ -3,18 +3,26 @@ package com.nukkitx.network.raknet;
 public interface RakMetrics {
 
     default void bytesIn(int count) {
-
     }
 
     default void bytesOut(int count) {
-
     }
 
-    default void packetsIn(int count) {
-
+    default void rakDatagramsIn(int count) {
     }
 
-    default void packetsOut(int count) {
+    default void rakDatagramsOut(int count) {
+    }
 
+    default void ackIn(int count) {
+    }
+
+    default void ackOut(int count) {
+    }
+
+    default void nackIn(int count) {
+    }
+
+    default void nackOut(int count) {
     }
 }
