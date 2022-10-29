27d9816f5b561884220ad1e56f08e9a781f8aa5e
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2022-10-29 23:39:09 +0100

    Stop messages from getting to the ServerBootstrap handler

---
+++ transport-raknet/src/main/java/org/cloudburstmc/netty/handler/codec/raknet/server/RakServerTailHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022 CloudburstMC
+ *
+ * CloudburstMC licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.cloudburstmc.netty.handler.codec.raknet.server;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+
+@Sharable
+public class RakServerTailHandler extends ChannelInboundHandlerAdapter {
+
+    public static final String NAME = "rak-server-tail-handler";
+
+    public static final RakServerTailHandler INSTANCE = new RakServerTailHandler();
+
+    private static final InternalLogger log = InternalLoggerFactory.getInstance(RakServerTailHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof Channel) {
+            super.channelRead(ctx, msg);
+        } else {
+            ReferenceCountUtil.release(msg);
+            log.debug("Received unexpected message in server channel: {}", msg);
+        }
+    }
+}
