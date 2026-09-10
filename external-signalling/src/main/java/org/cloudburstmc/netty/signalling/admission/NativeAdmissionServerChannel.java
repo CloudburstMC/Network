f2383dec96d1fc97812d7a85c7b066ffc3d9866f
irrld <irrelevantdev@outlook.com>
2026-09-10 20:39:49 +0300

    Move NetherNet admission into the external signalling module

--- external-signalling/src/main/java/org/cloudburstmc/netty/signalling/admission/NativeAdmissionServerChannel.java
+++ external-signalling/src/main/java/org/cloudburstmc/netty/signalling/admission/NativeAdmissionServerChannel.java
@@ -1,4 +1,4 @@
-package org.cloudburstmc.netty.channel.nethernet.admission;
+package org.cloudburstmc.netty.signalling.admission;
 
 import org.cloudburstmc.netty.channel.nethernet.config.DefaultNetherServerChannelConfig;
 import io.netty.channel.*;
