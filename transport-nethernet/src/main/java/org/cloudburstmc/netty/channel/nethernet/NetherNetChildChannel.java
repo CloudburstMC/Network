6d66b009a114c818cc67f19a2e896d5f706a0e9a
irrld <irrelevantdev@outlook.com>
2026-09-11 04:17:51 +0300

    Expose the signaling connection id on accepted child channels

--- transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChildChannel.java
+++ transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChildChannel.java
@@ -3,12 +3,23 @@ package org.cloudburstmc.netty.channel.nethernet;
 import org.cloudburstmc.netty.channel.nethernet.config.DefaultNetherChannelConfig;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelPromise;
+import io.netty.util.AttributeKey;
 import tel.schich.libdatachannel.PeerConnection;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 public class NetherNetChildChannel extends NetherNetChannel {
+
+    /**
+     * The signaling connection ID this channel was accepted for.
+     * <p>
+     * A child arrives on the server pipeline carrying nothing that ties it back to the offer that
+     * produced it, and arrival order does not follow the order answers were produced in.
+     */
+    public static final AttributeKey<Long> CONNECTION_ID =
+            AttributeKey.valueOf(NetherNetChildChannel.class, "connectionId");
+
     public NetherNetChildChannel(Channel parent, PeerConnection peerConnection, InetSocketAddress remote,
                                  InetSocketAddress local) {
         super(parent, remote, local);
