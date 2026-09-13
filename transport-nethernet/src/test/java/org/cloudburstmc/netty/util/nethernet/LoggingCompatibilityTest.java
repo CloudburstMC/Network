EduGeyser/NetworkM

Refresh transport dependencies and logging integration
Declare updated SLF4J and annotation versions for Maven consumers as well as Gradle. Keep the logging backend in test scope and cover logging and signed identity serialization.

Validation: 173 tests passed on Java 21; 36 NetherNet tests passed on each of Java 25 and 26. Maven dependency resolution and workflow lint passed.

transport-nethernet/src/test/java/dev/kastle/netty/util/nethernet/LoggingCompatibilityTest.java
@@ -1,0 +1,51 @@
+package dev.kastle.netty.util.nethernet;
+
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoggingCompatibilityTest {
+    @Test
+    void slf4jAndNettyLogsReachTheConfiguredBackend() {
+        String name = getClass().getName();
+        Logger backend = Logger.getLogger(name);
+        Level previousLevel = backend.getLevel();
+        boolean previousParentHandlers = backend.getUseParentHandlers();
+        List<String> messages = new ArrayList<>();
+        Handler capture = new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                messages.add(record.getMessage());
+            }
+
+            @Override
+            public void flush() {
+            }
+
+            @Override
+            public void close() {
+            }
+        };
+        backend.setLevel(Level.ALL);
+        backend.setUseParentHandlers(false);
+        backend.addHandler(capture);
+        try {
+            LoggerFactory.getLogger(name).atInfo().addArgument("message").log("SLF4J {}");
+            InternalLoggerFactory.getInstance(name).info("Netty {}", "message");
+            assertEquals(List.of("SLF4J message", "Netty message"), messages);
+        } finally {
+            backend.removeHandler(capture);
+            backend.setLevel(previousLevel);
+            backend.setUseParentHandlers(previousParentHandlers);
+        }
+    }
+}

transport-nethernet/src/test/java/dev/kastle/netty/util/nethernet/ServerIdentityTest.java
@@ -1,0 +1,50 @@
+package dev.kastle.netty.util.nethernet;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.jose4j.jwk.EcJwkGenerator;
+import org.jose4j.jwk.EllipticCurveJsonWebKey;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.keys.EllipticCurves;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ServerIdentityTest {
+    @Test
+    void serializedIdentityPreservesBothSignatures() throws Exception {
+        EllipticCurveJsonWebKey key = EcJwkGenerator.generateJwk(EllipticCurves.P384);
+        Instant expiry = Instant.now().plusSeconds(3600);
+        ServerIdentity identity = new ServerIdentity(key.getPrivateKey(), key.getPublicKey(), expiry, "test.invalid");
+        String digest = "AA:".repeat(31) + "AA";
+        String sdp = "v=0\r\na=fingerprint:sha-256 " + digest + "\r\nm=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\n";
+
+        String json = new String(Base64.getDecoder().decode(identity.identityValue(sdp)), StandardCharsets.UTF_8);
+        JsonObject envelope = JsonParser.parseString(json).getAsJsonObject();
+        assertEquals("test.invalid", envelope.getAsJsonObject("idp").get("domain").getAsString());
+        JsonObject assertion = JsonParser.parseString(envelope.get("assertion").getAsString()).getAsJsonObject();
+
+        JsonWebSignature token = new JsonWebSignature();
+        token.setCompactSerialization(assertion.get("token").getAsString());
+        token.setKey(key.getPublicKey());
+        assertEquals(AlgorithmIdentifiers.ECDSA_USING_P384_CURVE_AND_SHA384, token.getAlgorithmHeaderValue());
+        assertTrue(token.verifySignature());
+        JwtClaims claims = JwtClaims.parse(token.getPayload());
+        assertEquals("test.invalid", claims.getIssuer());
+        assertEquals(expiry.getEpochSecond(), claims.getExpirationTime().getValue());
+        assertEquals(Base64.getEncoder().encodeToString(key.getPublicKey().getEncoded()), claims.getStringClaimValue("cpk"));
+
+        JsonWebSignature fingerprints = new JsonWebSignature();
+        fingerprints.setCompactSerialization(assertion.get("fingerprints").getAsString());
+        fingerprints.setPayload("{\"fingerprint\":[{\"algorithm\":\"sha-256\",\"digest\":\"" + digest + "\"}]}");
+        fingerprints.setKey(key.getPublicKey());
+        assertTrue(fingerprints.verifySignature());
+    }
+}

CloudburstMC/Network

transport-nethernet/src/test/java/org/cloudburstmc/netty/util/nethernet/LoggingCompatibilityTest.java
@@ -1,0 +1,51 @@
+package org.cloudburstmc.netty.util.nethernet;
+
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoggingCompatibilityTest {
+    @Test
+    void slf4jAndNettyLogsReachTheConfiguredBackend() {
+        String name = getClass().getName();
+        Logger backend = Logger.getLogger(name);
+        Level previousLevel = backend.getLevel();
+        boolean previousParentHandlers = backend.getUseParentHandlers();
+        List<String> messages = new ArrayList<>();
+        Handler capture = new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                messages.add(record.getMessage());
+            }
+
+            @Override
+            public void flush() {
+            }
+
+            @Override
+            public void close() {
+            }
+        };
+        backend.setLevel(Level.ALL);
+        backend.setUseParentHandlers(false);
+        backend.addHandler(capture);
+        try {
+            LoggerFactory.getLogger(name).atInfo().addArgument("message").log("SLF4J {}");
+            InternalLoggerFactory.getInstance(name).info("Netty {}", "message");
+            assertEquals(List.of("SLF4J message", "Netty message"), messages);
+        } finally {
+            backend.removeHandler(capture);
+            backend.setLevel(previousLevel);
+            backend.setUseParentHandlers(previousParentHandlers);
+        }
+    }
+}

transport-nethernet/src/test/java/org/cloudburstmc/netty/util/nethernet/ServerIdentityTest.java
@@ -1,0 +1,50 @@
+package org.cloudburstmc.netty.util.nethernet;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.jose4j.jwk.EcJwkGenerator;
+import org.jose4j.jwk.EllipticCurveJsonWebKey;
+import org.jose4j.jws.AlgorithmIdentifiers;
+import org.jose4j.jws.JsonWebSignature;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.keys.EllipticCurves;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Base64;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ServerIdentityTest {
+    @Test
+    void serializedIdentityPreservesBothSignatures() throws Exception {
+        EllipticCurveJsonWebKey key = EcJwkGenerator.generateJwk(EllipticCurves.P384);
+        Instant expiry = Instant.now().plusSeconds(3600);
+        ServerIdentity identity = new ServerIdentity(key.getPrivateKey(), key.getPublicKey(), expiry, "test.invalid");
+        String digest = "AA:".repeat(31) + "AA";
+        String sdp = "v=0\r\na=fingerprint:sha-256 " + digest + "\r\nm=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\n";
+
+        String json = new String(Base64.getDecoder().decode(identity.identityValue(sdp)), StandardCharsets.UTF_8);
+        JsonObject envelope = JsonParser.parseString(json).getAsJsonObject();
+        assertEquals("test.invalid", envelope.getAsJsonObject("idp").get("domain").getAsString());
+        JsonObject assertion = JsonParser.parseString(envelope.get("assertion").getAsString()).getAsJsonObject();
+
+        JsonWebSignature token = new JsonWebSignature();
+        token.setCompactSerialization(assertion.get("token").getAsString());
+        token.setKey(key.getPublicKey());
+        assertEquals(AlgorithmIdentifiers.ECDSA_USING_P384_CURVE_AND_SHA384, token.getAlgorithmHeaderValue());
+        assertTrue(token.verifySignature());
+        JwtClaims claims = JwtClaims.parse(token.getPayload());
+        assertEquals("test.invalid", claims.getIssuer());
+        assertEquals(expiry.getEpochSecond(), claims.getExpirationTime().getValue());
+        assertEquals(Base64.getEncoder().encodeToString(key.getPublicKey().getEncoded()), claims.getStringClaimValue("cpk"));
+
+        JsonWebSignature fingerprints = new JsonWebSignature();
+        fingerprints.setCompactSerialization(assertion.get("fingerprints").getAsString());
+        fingerprints.setPayload("{\"fingerprint\":[{\"algorithm\":\"sha-256\",\"digest\":\"" + digest + "\"}]}");
+        fingerprints.setKey(key.getPublicKey());
+        assertTrue(fingerprints.verifySignature());
+    }
+}