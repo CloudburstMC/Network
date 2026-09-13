SendableMetatype/NetworkCompatible

Isolate libwebrtc behind a backend seam and framing codec
The server data plane no longer reaches into libwebrtc. A
WebRtcServerBackend interface owns offer negotiation and data channel
transfer, with LibWebRtcServerBackend as the single class touching
engine types: factory pooling, port allocator defaults, session
tracking, and a lifecycle lock that makes close idempotent and safe
against in flight accepts, closing every live session before disposing
the factories they run on. Child channels attach to backend sessions
and the server channel bridges session events into netty.

NetherNet's countdown framing moves out of the channels into
NetherNetFramingCodec, a standalone duplex handler placed first in the
pipeline. Channels now move raw fragments; the codec fragments to the
negotiated maximum message size and reassembles with a 16MB cap on
both the accumulation and completion paths. Covered by unit tests.

The client channel keeps direct engine access but gains the same
hardening sent to the upstream PR branch: handshake retries are
attempt scoped so stale engine callbacks cannot mutate a replacement
attempt, signal ids are re validated on the event loop, and remote
candidates buffer until the answer is applied. JSON RPC requests are
tagged with the socket that carried them so a reconnect only fails its
own requests.

The server handshake reaper is now cancelled by the child's close
future rather than from engine observer callbacks, so it fires only
for a connection that is still open but never activated.

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetChannelFactory.java
@@ -1,5 +1,6 @@
 package dev.kastle.netty.channel.nethernet;
 
+import dev.kastle.netty.channel.nethernet.backend.WebRtcServerBackend;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetClientSignaling;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetServerSignaling;
 import dev.kastle.webrtc.PeerConnectionFactory;
@@ -48,6 +49,19 @@ public class NetherNetChannelFactory<T extends Channel> implements ChannelFactor
      */
     public static ChannelFactory<NetherNetServerChannel> server(List<PeerConnectionFactory> factories, NetherNetServerSignaling signaling) {
         return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(factories, signaling));
+    }
+
+    /**
+     * Creates a NetherNet Server Channel Factory over an explicit WebRTC
+     * backend. The resulting server channel takes ownership of the backend
+     * and closes it on close.
+     *
+     * @param backend   The backend negotiating and carrying connections.
+     * @param signaling The NetherNetServerSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetServerChannel.
+     */
+    public static ChannelFactory<NetherNetServerChannel> server(WebRtcServerBackend backend, NetherNetServerSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(backend, signaling));
     }
 
     /**

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetChildChannel.java
@@ -1,18 +1,44 @@
 package dev.kastle.netty.channel.nethernet;
 
+import dev.kastle.netty.channel.nethernet.backend.WebRtcSession;
 import dev.kastle.netty.channel.nethernet.config.DefaultNetherChannelConfig;
-import dev.kastle.webrtc.RTCPeerConnection;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelPromise;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
+/**
+ * A server accepted NetherNet connection, backed by a
+ * {@link WebRtcSession} from the backend seam. The parent server channel
+ * bridges session events (open, close, inbound messages, remote address)
+ * into this channel.
+ */
 public class NetherNetChildChannel extends NetherNetChannel {
-    public NetherNetChildChannel(Channel parent, RTCPeerConnection peerConnection, InetSocketAddress remote, InetSocketAddress local) {
+
+    private volatile WebRtcSession session;
+
+    public NetherNetChildChannel(Channel parent, InetSocketAddress remote, InetSocketAddress local) {
         super(parent, remote, local);
-        this.peerConnection = peerConnection;
         this.config = new DefaultNetherChannelConfig(this);
+    }
+
+    /**
+     * Attaches the negotiated session. Called once by the server channel
+     * right after the backend accepts the offer, before the transport can
+     * open.
+     */
+    public void attachSession(WebRtcSession session) {
+        this.session = session;
+    }
+
+    @Override
+    protected void sendFramed(ByteBuf framed) {
+        WebRtcSession session = this.session;
+        if (session != null) {
+            session.send(toNioBuffer(framed));
+        }
     }
 
     @Override
@@ -28,5 +54,14 @@ public class NetherNetChildChannel extends NetherNetChannel {
     @Override
     protected void doBind(SocketAddress localAddress) throws Exception {
         throw new UnsupportedOperationException("Child channel cannot be bound");
+    }
+
+    @Override
+    protected void doClose() throws Exception {
+        super.doClose();
+        WebRtcSession session = this.session;
+        if (session != null) {
+            session.close();
+        }
     }
 }

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/backend/WebRtcServerBackend.java
@@ -1,0 +1,38 @@
+package dev.kastle.netty.channel.nethernet.backend;
+
+import dev.kastle.netty.channel.nethernet.signaling.NetherNetSignaling.IceServerInfo;
+
+import java.util.List;
+
+/**
+ * The seam between the NetherNet transport and a WebRTC engine. Everything the
+ * transport needs from WebRTC goes through this interface and its companions
+ * {@link WebRtcSession} and {@link WebRtcSessionListener}; no WebRTC engine
+ * types appear above it. NetherNet uses a deliberately tiny slice of WebRTC
+ * (data channels only, no media, no renegotiation), which keeps this surface
+ * small, makes the transport testable without native libraries, and keeps a
+ * future engine swap contained to one implementation class.
+ */
+public interface WebRtcServerBackend extends AutoCloseable {
+
+    /**
+     * Accepts an incoming connection offer and starts negotiating a session
+     * for it. Returns immediately; negotiation progress, inbound data, and
+     * lifecycle events are reported through the listener, potentially from
+     * engine threads.
+     *
+     * @param offerSdp   the remote peer's SDP offer
+     * @param iceServers STUN/TURN servers to use for this session, may be
+     *                   empty
+     * @param listener   receives negotiation and data events for the session
+     * @return the session handle
+     */
+    WebRtcSession accept(String offerSdp, List<IceServerInfo> iceServers, WebRtcSessionListener listener);
+
+    /**
+     * Releases every engine resource this backend owns. Live sessions are
+     * closed. Idempotent.
+     */
+    @Override
+    void close();
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/backend/WebRtcSession.java
@@ -1,0 +1,40 @@
+package dev.kastle.netty.channel.nethernet.backend;
+
+import java.nio.ByteBuffer;
+
+/**
+ * One negotiated WebRTC connection as seen by the NetherNet transport: a
+ * reliable ordered byte-message stream plus lifecycle control. Counterpart
+ * events arrive on the {@link WebRtcSessionListener} passed to
+ * {@link WebRtcServerBackend#accept}.
+ */
+public interface WebRtcSession {
+
+    /**
+     * Queues the bytes between the buffer's position and limit for
+     * transmission on the reliable data channel without blocking on the
+     * engine's network processing. The data is copied out of the buffer
+     * before this method returns, so the caller may reuse the buffer
+     * immediately. Messages sent before the transport is open, or after it
+     * has closed, are dropped.
+     *
+     * @param data the message payload, already NetherNet framed
+     */
+    void send(ByteBuffer data);
+
+    /**
+     * Applies a remote ICE candidate signaled for this session. Candidates
+     * received before negotiation has applied the remote description are
+     * buffered and applied in order once it has; callers may hand candidates
+     * over as they arrive without worrying about negotiation timing.
+     *
+     * @param candidateSdp the candidate SDP string
+     */
+    void addRemoteCandidate(String candidateSdp);
+
+    /**
+     * Tears the session down. Idempotent; does not fire
+     * {@link WebRtcSessionListener#onTransportClosed()}.
+     */
+    void close();
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/backend/WebRtcSessionListener.java
@@ -1,0 +1,74 @@
+package dev.kastle.netty.channel.nethernet.backend;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+/**
+ * Receives negotiation, data, and lifecycle events for one
+ * {@link WebRtcSession}. Callbacks are invoked from engine threads and must
+ * return promptly; heavy work belongs on the consumer's own executor.
+ */
+public interface WebRtcSessionListener {
+
+    /**
+     * The local SDP answer is ready to be signaled back to the remote peer.
+     * Local ICE candidates follow via {@link #onLocalCandidate(String)}.
+     *
+     * @param answerSdp the local SDP answer
+     */
+    void onAnswerReady(String answerSdp);
+
+    /**
+     * A local ICE candidate was gathered and should be signaled to the
+     * remote peer (trickle ICE).
+     *
+     * @param candidateSdp the candidate SDP string
+     */
+    void onLocalCandidate(String candidateSdp);
+
+    /**
+     * The data channels are open; the session can now carry traffic.
+     */
+    void onTransportOpen();
+
+    /**
+     * A message arrived on the reliable data channel, still carrying its
+     * NetherNet framing header. The buffer wraps engine memory that is only
+     * valid for the duration of this callback; the consumer must copy the
+     * bytes before returning.
+     *
+     * @param data the raw framed message
+     */
+    void onMessage(ByteBuffer data);
+
+    /**
+     * ICE selected (or re-selected) a candidate pair; the given address is
+     * the remote transport address actually exchanging packets with us. For
+     * relayed connections this is the TURN relay. Fires before
+     * {@link #onTransportOpen()} on current engines, and may fire again on
+     * re-nomination.
+     *
+     * @param address       the remote candidate's address
+     * @param candidateType the remote candidate type: host, srflx, prflx, or relay
+     */
+    void onRemoteAddress(InetSocketAddress address, String candidateType);
+
+    /**
+     * The engine wrote previously queued message bytes to the wire, shrinking
+     * its internal send buffer by the given amount. Consumers use this to
+     * bound how much data they queue into the engine
+     * (see {@link WebRtcSession#send}): pause above a high water mark of
+     * unsent bytes, resume when this callback drains below a low water mark.
+     * Default is a no-op for consumers that do not track engine buffering.
+     *
+     * @param bytes the number of buffered bytes handed to the wire
+     */
+    default void onBytesSent(long bytes) {
+    }
+
+    /**
+     * The transport failed or was closed by the remote peer. Not fired for
+     * local {@link WebRtcSession#close()} calls.
+     */
+    void onTransportClosed();
+}

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChannelFactory.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.netty.channel.nethernet;
 
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcServerBackend;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetClientSignaling;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetServerSignaling;
 import dev.kastle.webrtc.PeerConnectionFactory;
@@ -48,6 +49,19 @@ public class NetherNetChannelFactory<T extends Channel> implements ChannelFactor
      */
     public static ChannelFactory<NetherNetServerChannel> server(List<PeerConnectionFactory> factories, NetherNetServerSignaling signaling) {
         return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(factories, signaling));
+    }
+
+    /**
+     * Creates a NetherNet Server Channel Factory over an explicit WebRTC
+     * backend. The resulting server channel takes ownership of the backend
+     * and closes it on close.
+     *
+     * @param backend   The backend negotiating and carrying connections.
+     * @param signaling The NetherNetServerSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetServerChannel.
+     */
+    public static ChannelFactory<NetherNetServerChannel> server(WebRtcServerBackend backend, NetherNetServerSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(backend, signaling));
     }
 
     /**

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChildChannel.java
@@ -1,18 +1,44 @@
 package org.cloudburstmc.netty.channel.nethernet;
 
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSession;
 import org.cloudburstmc.netty.channel.nethernet.config.DefaultNetherChannelConfig;
-import dev.kastle.webrtc.RTCPeerConnection;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelPromise;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
+/**
+ * A server accepted NetherNet connection, backed by a
+ * {@link WebRtcSession} from the backend seam. The parent server channel
+ * bridges session events (open, close, inbound messages, remote address)
+ * into this channel.
+ */
 public class NetherNetChildChannel extends NetherNetChannel {
-    public NetherNetChildChannel(Channel parent, RTCPeerConnection peerConnection, InetSocketAddress remote, InetSocketAddress local) {
+
+    private volatile WebRtcSession session;
+
+    public NetherNetChildChannel(Channel parent, InetSocketAddress remote, InetSocketAddress local) {
         super(parent, remote, local);
-        this.peerConnection = peerConnection;
         this.config = new DefaultNetherChannelConfig(this);
+    }
+
+    /**
+     * Attaches the negotiated session. Called once by the server channel
+     * right after the backend accepts the offer, before the transport can
+     * open.
+     */
+    public void attachSession(WebRtcSession session) {
+        this.session = session;
+    }
+
+    @Override
+    protected void sendFramed(ByteBuf framed) {
+        WebRtcSession session = this.session;
+        if (session != null) {
+            session.send(toNioBuffer(framed));
+        }
     }
 
     @Override
@@ -28,5 +54,14 @@ public class NetherNetChildChannel extends NetherNetChannel {
     @Override
     protected void doBind(SocketAddress localAddress) throws Exception {
         throw new UnsupportedOperationException("Child channel cannot be bound");
+    }
+
+    @Override
+    protected void doClose() throws Exception {
+        super.doClose();
+        WebRtcSession session = this.session;
+        if (session != null) {
+            session.close();
+        }
     }
 }

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/backend/WebRtcServerBackend.java
@@ -1,0 +1,38 @@
+package org.cloudburstmc.netty.channel.nethernet.backend;
+
+import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetSignaling.IceServerInfo;
+
+import java.util.List;
+
+/**
+ * The seam between the NetherNet transport and a WebRTC engine. Everything the
+ * transport needs from WebRTC goes through this interface and its companions
+ * {@link WebRtcSession} and {@link WebRtcSessionListener}; no WebRTC engine
+ * types appear above it. NetherNet uses a deliberately tiny slice of WebRTC
+ * (data channels only, no media, no renegotiation), which keeps this surface
+ * small, makes the transport testable without native libraries, and keeps a
+ * future engine swap contained to one implementation class.
+ */
+public interface WebRtcServerBackend extends AutoCloseable {
+
+    /**
+     * Accepts an incoming connection offer and starts negotiating a session
+     * for it. Returns immediately; negotiation progress, inbound data, and
+     * lifecycle events are reported through the listener, potentially from
+     * engine threads.
+     *
+     * @param offerSdp   the remote peer's SDP offer
+     * @param iceServers STUN/TURN servers to use for this session, may be
+     *                   empty
+     * @param listener   receives negotiation and data events for the session
+     * @return the session handle
+     */
+    WebRtcSession accept(String offerSdp, List<IceServerInfo> iceServers, WebRtcSessionListener listener);
+
+    /**
+     * Releases every engine resource this backend owns. Live sessions are
+     * closed. Idempotent.
+     */
+    @Override
+    void close();
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/backend/WebRtcSession.java
@@ -1,0 +1,40 @@
+package org.cloudburstmc.netty.channel.nethernet.backend;
+
+import java.nio.ByteBuffer;
+
+/**
+ * One negotiated WebRTC connection as seen by the NetherNet transport: a
+ * reliable ordered byte-message stream plus lifecycle control. Counterpart
+ * events arrive on the {@link WebRtcSessionListener} passed to
+ * {@link WebRtcServerBackend#accept}.
+ */
+public interface WebRtcSession {
+
+    /**
+     * Queues the bytes between the buffer's position and limit for
+     * transmission on the reliable data channel without blocking on the
+     * engine's network processing. The data is copied out of the buffer
+     * before this method returns, so the caller may reuse the buffer
+     * immediately. Messages sent before the transport is open, or after it
+     * has closed, are dropped.
+     *
+     * @param data the message payload, already NetherNet framed
+     */
+    void send(ByteBuffer data);
+
+    /**
+     * Applies a remote ICE candidate signaled for this session. Candidates
+     * received before negotiation has applied the remote description are
+     * buffered and applied in order once it has; callers may hand candidates
+     * over as they arrive without worrying about negotiation timing.
+     *
+     * @param candidateSdp the candidate SDP string
+     */
+    void addRemoteCandidate(String candidateSdp);
+
+    /**
+     * Tears the session down. Idempotent; does not fire
+     * {@link WebRtcSessionListener#onTransportClosed()}.
+     */
+    void close();
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/backend/WebRtcSessionListener.java
@@ -1,0 +1,74 @@
+package org.cloudburstmc.netty.channel.nethernet.backend;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+/**
+ * Receives negotiation, data, and lifecycle events for one
+ * {@link WebRtcSession}. Callbacks are invoked from engine threads and must
+ * return promptly; heavy work belongs on the consumer's own executor.
+ */
+public interface WebRtcSessionListener {
+
+    /**
+     * The local SDP answer is ready to be signaled back to the remote peer.
+     * Local ICE candidates follow via {@link #onLocalCandidate(String)}.
+     *
+     * @param answerSdp the local SDP answer
+     */
+    void onAnswerReady(String answerSdp);
+
+    /**
+     * A local ICE candidate was gathered and should be signaled to the
+     * remote peer (trickle ICE).
+     *
+     * @param candidateSdp the candidate SDP string
+     */
+    void onLocalCandidate(String candidateSdp);
+
+    /**
+     * The data channels are open; the session can now carry traffic.
+     */
+    void onTransportOpen();
+
+    /**
+     * A message arrived on the reliable data channel, still carrying its
+     * NetherNet framing header. The buffer wraps engine memory that is only
+     * valid for the duration of this callback; the consumer must copy the
+     * bytes before returning.
+     *
+     * @param data the raw framed message
+     */
+    void onMessage(ByteBuffer data);
+
+    /**
+     * ICE selected (or re-selected) a candidate pair; the given address is
+     * the remote transport address actually exchanging packets with us. For
+     * relayed connections this is the TURN relay. Fires before
+     * {@link #onTransportOpen()} on current engines, and may fire again on
+     * re-nomination.
+     *
+     * @param address       the remote candidate's address
+     * @param candidateType the remote candidate type: host, srflx, prflx, or relay
+     */
+    void onRemoteAddress(InetSocketAddress address, String candidateType);
+
+    /**
+     * The engine wrote previously queued message bytes to the wire, shrinking
+     * its internal send buffer by the given amount. Consumers use this to
+     * bound how much data they queue into the engine
+     * (see {@link WebRtcSession#send}): pause above a high water mark of
+     * unsent bytes, resume when this callback drains below a low water mark.
+     * Default is a no-op for consumers that do not track engine buffering.
+     *
+     * @param bytes the number of buffered bytes handed to the wire
+     */
+    default void onBytesSent(long bytes) {
+    }
+
+    /**
+     * The transport failed or was closed by the remote peer. Not fired for
+     * local {@link WebRtcSession#close()} calls.
+     */
+    void onTransportClosed();
+}