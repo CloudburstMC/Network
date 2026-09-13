SendableMetatype/NetworkCompatible

Add the HTTP signaling front end with full ICE answers
NetherNetHttpSignaling serves NetherNet's direct connection model on
TCP under the Bedrock port: GET /v1/join as the capability check and
POST /v1/join/{networkId} exchanging the SDP offer for a full ICE
answer in one round trip, per Mojang's onboarding guide and matching
the go-nethernet reference (one shot connections, 1 MiB offer cap,
uint64 network id validation, 502 after the negotiation timeout with
the half negotiated child reaped). The listener owns a single thread
accept group because bind() runs on the server channel's event loop.

The backend gains a full ICE accept mode: no trickle candidates in
either direction, the answer reported once after the local description
applies and gathering completes, re read from the engine so it carries
every candidate, and terminated with a=end-of-candidates.

Server signaling implementations can now report the peer's transport
address (the HTTP front end knows it from the request) used as the
child's initial remote address, and request full ICE answers. A new
NETHER_SERVER_ANSWER_DECORATOR channel option lets consumers transform
answers before signaling, the seam for the server identity assertion
that HTTP signaled clients require; TLS is a Supplier<SslContext>
consulted per connection so certificate rotation needs no rebind.

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetAnswerDecorator.java
@@ -1,0 +1,26 @@
+package dev.kastle.netty.channel.nethernet;
+
+/**
+ * Transforms an SDP answer before it is signaled back to the remote peer.
+ * The primary use is attaching the server identity assertion (the
+ * {@code a=identity} attribute) that clients reached over HTTP signaling
+ * require in every answer; consumers own the assertion format and keys,
+ * the transport only provides this seam.
+ *
+ * Called from engine threads; implementations must be thread safe and
+ * return promptly. A thrown exception makes the channel fall back to the
+ * undecorated answer rather than dropping the exchange.
+ */
+@FunctionalInterface
+public interface NetherNetAnswerDecorator {
+
+    /**
+     * Returns the answer to actually signal, derived from the negotiated
+     * answer.
+     *
+     * @param answerSdp the negotiated SDP answer
+     * @return the SDP answer to signal to the peer
+     * @throws Exception if decoration fails; the undecorated answer is used
+     */
+    String decorate(String answerSdp) throws Exception;
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/config/DefaultNetherServerChannelConfig.java
@@ -1,5 +1,6 @@
 package dev.kastle.netty.channel.nethernet.config;
 
+import dev.kastle.netty.channel.nethernet.NetherNetAnswerDecorator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
 
@@ -7,6 +8,7 @@ import java.util.Map;
 
 public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig  {
     private volatile int serverRtcHandshakeTimeoutSeconds = 30;
+    private volatile NetherNetAnswerDecorator answerDecorator;
 
     public DefaultNetherServerChannelConfig(Channel channel) {
         super(channel);
@@ -15,7 +17,8 @@ public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig
     @Override
     public Map<ChannelOption<?>, Object> getOptions() {
         return this.getOptions(
-                super.getOptions(), NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS
+                super.getOptions(), NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS,
+                NetherChannelOption.NETHER_SERVER_ANSWER_DECORATOR
         );
     }
 
@@ -24,6 +27,9 @@ public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig
     public <T> T getOption(ChannelOption<T> option) {
         if (option == NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS) {
             return (T) Integer.valueOf(this.serverRtcHandshakeTimeoutSeconds);
+        }
+        if (option == NetherChannelOption.NETHER_SERVER_ANSWER_DECORATOR) {
+            return (T) this.answerDecorator;
         }
 
         return super.getOption(option);
@@ -35,6 +41,9 @@ public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig
 
         if (option == NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS) {
             this.setServerRtcHandshakeTimeoutSeconds((Integer) value);
+            return true;
+        } else if (option == NetherChannelOption.NETHER_SERVER_ANSWER_DECORATOR) {
+            this.answerDecorator = (NetherNetAnswerDecorator) value;
             return true;
         } else {
             return super.setOption(option, value);

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/config/NetherChannelOption.java
@@ -1,9 +1,18 @@
 package dev.kastle.netty.channel.nethernet.config;
 
+import dev.kastle.netty.channel.nethernet.NetherNetAnswerDecorator;
 import dev.kastle.webrtc.PortAllocatorConfig;
 import io.netty.channel.ChannelOption;
 
 public class NetherChannelOption<T> extends ChannelOption<T> {
+
+    /**
+     * Transforms every SDP answer before it is signaled, e.g. to attach the
+     * server identity assertion. Null (the default) signals answers as
+     * negotiated.
+     */
+    public static final ChannelOption<NetherNetAnswerDecorator> NETHER_SERVER_ANSWER_DECORATOR =
+            valueOf(NetherChannelOption.class, "NETHER_SERVER_ANSWER_DECORATOR");
 
     /**
      * The PortAllocatorConfig used for WebRTC connections.

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/signaling/NetherNetHttpSignalingTest.java
@@ -1,0 +1,148 @@
+package dev.kastle.netty.channel.nethernet.signaling;
+
+import dev.kastle.netty.channel.nethernet.NetherNetServerChannel;
+import dev.kastle.netty.channel.nethernet.backend.WebRtcServerBackend;
+import dev.kastle.netty.channel.nethernet.backend.WebRtcSession;
+import dev.kastle.netty.channel.nethernet.backend.WebRtcSessionListener;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Exercises the HTTP signaling front end against the real netty stack and
+ * server channel, with the WebRTC engine replaced by a scripted backend: an
+ * accepted offer produces a canned full ICE answer on a separate thread,
+ * mirroring how the engine reports the answer after gathering completes.
+ */
+class NetherNetHttpSignalingTest {
+
+    private static final String CANNED_ANSWER = "v=0\r\no=- 0 2 IN IP4 127.0.0.1\r\na=candidate:1 1 udp 2122260223 127.0.0.1 55555 typ host\r\n";
+
+    private static NioEventLoopGroup group;
+    private static NetherNetHttpSignaling signaling;
+    private static Channel serverChannel;
+    private static ScriptedBackend backend;
+    private static HttpClient client;
+    private static String baseUrl;
+
+    /** Records accepts and answers each offer with the canned SDP. */
+    private static final class ScriptedBackend implements WebRtcServerBackend {
+        final List<Boolean> fullIceFlags = new CopyOnWriteArrayList<>();
+        volatile boolean answerOffers = true;
+
+        @Override
+        public WebRtcSession accept(String offerSdp, List<NetherNetSignaling.IceServerInfo> iceServers,
+                                    WebRtcSessionListener listener, boolean fullIceAnswer) {
+            fullIceFlags.add(fullIceAnswer);
+            if (answerOffers) {
+                // Engine threads report the answer asynchronously; model that.
+                CompletableFuture.runAsync(() -> listener.onAnswerReady(CANNED_ANSWER));
+            }
+            return new WebRtcSession() {
+                @Override
+                public void send(ByteBuffer data) {
+                }
+
+                @Override
+                public void addRemoteCandidate(String candidateSdp) {
+                }
+
+                @Override
+                public void close() {
+                }
+            };
+        }
+
+        @Override
+        public void close() {
+        }
+    }
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        group = new NioEventLoopGroup(2);
+        backend = new ScriptedBackend();
+        signaling = new NetherNetHttpSignaling(null, group);
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(group, group)
+                .channelFactory(() -> new NetherNetServerChannel(backend, signaling))
+                .childHandler(new ChannelInboundHandlerAdapter());
+        serverChannel = bootstrap.bind(new InetSocketAddress("127.0.0.1", 0)).sync().channel();
+
+        InetSocketAddress bound = signaling.boundAddress();
+        baseUrl = "http://127.0.0.1:" + bound.getPort();
+        client = HttpClient.newHttpClient();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (serverChannel != null) {
+            serverChannel.close().syncUninterruptibly();
+        }
+        if (group != null) {
+            group.shutdownGracefully(0, 3, TimeUnit.SECONDS).syncUninterruptibly();
+        }
+    }
+
+    private static HttpResponse<String> get(String path) throws Exception {
+        return client.send(HttpRequest.newBuilder(URI.create(baseUrl + path)).GET().build(),
+                HttpResponse.BodyHandlers.ofString());
+    }
+
+    private static HttpResponse<String> post(String path, String body) throws Exception {
+        return client.send(HttpRequest.newBuilder(URI.create(baseUrl + path))
+                        .POST(HttpRequest.BodyPublishers.ofString(body)).build(),
+                HttpResponse.BodyHandlers.ofString());
+    }
+
+    @Test
+    void joinProbeAnswers200() throws Exception {
+        assertEquals(200, get("/v1/join").statusCode());
+    }
+
+    @Test
+    void unknownPathAnswers404() throws Exception {
+        assertEquals(404, get("/v1/other").statusCode());
+        assertEquals(404, post("/v2/join/123", "x").statusCode());
+    }
+
+    @Test
+    void offerProducesFullIceAnswer() throws Exception {
+        HttpResponse<String> response = post("/v1/join/12345678901234567890", "v=0\r\nfake offer\r\n");
+        assertEquals(200, response.statusCode());
+        assertEquals("application/sdp", response.headers().firstValue("content-type").orElse(""));
+        assertEquals(CANNED_ANSWER, response.body());
+        assertTrue(response.headers().firstValue("connection").orElse("").equalsIgnoreCase("close"));
+        // The channel must have requested a full ICE answer from the backend.
+        assertTrue(backend.fullIceFlags.stream().allMatch(Boolean::booleanValue));
+    }
+
+    @Test
+    void nonNumericNetworkIdAnswers400() throws Exception {
+        assertEquals(400, post("/v1/join/not-a-number", "v=0\r\n").statusCode());
+    }
+
+    @Test
+    void emptyOfferAnswers400() throws Exception {
+        assertEquals(400, post("/v1/join/42", "").statusCode());
+    }
+}

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetAnswerDecorator.java
@@ -1,0 +1,26 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+/**
+ * Transforms an SDP answer before it is signaled back to the remote peer.
+ * The primary use is attaching the server identity assertion (the
+ * {@code a=identity} attribute) that clients reached over HTTP signaling
+ * require in every answer; consumers own the assertion format and keys,
+ * the transport only provides this seam.
+ *
+ * Called from engine threads; implementations must be thread safe and
+ * return promptly. A thrown exception makes the channel fall back to the
+ * undecorated answer rather than dropping the exchange.
+ */
+@FunctionalInterface
+public interface NetherNetAnswerDecorator {
+
+    /**
+     * Returns the answer to actually signal, derived from the negotiated
+     * answer.
+     *
+     * @param answerSdp the negotiated SDP answer
+     * @return the SDP answer to signal to the peer
+     * @throws Exception if decoration fails; the undecorated answer is used
+     */
+    String decorate(String answerSdp) throws Exception;
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/config/DefaultNetherServerChannelConfig.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.netty.channel.nethernet.config;
 
+import org.cloudburstmc.netty.channel.nethernet.NetherNetAnswerDecorator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
 
@@ -7,6 +8,7 @@ import java.util.Map;
 
 public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig  {
     private volatile int serverRtcHandshakeTimeoutSeconds = 30;
+    private volatile NetherNetAnswerDecorator answerDecorator;
 
     public DefaultNetherServerChannelConfig(Channel channel) {
         super(channel);
@@ -15,7 +17,8 @@ public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig
     @Override
     public Map<ChannelOption<?>, Object> getOptions() {
         return this.getOptions(
-                super.getOptions(), NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS
+                super.getOptions(), NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS,
+                NetherChannelOption.NETHER_SERVER_ANSWER_DECORATOR
         );
     }
 
@@ -24,6 +27,9 @@ public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig
     public <T> T getOption(ChannelOption<T> option) {
         if (option == NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS) {
             return (T) Integer.valueOf(this.serverRtcHandshakeTimeoutSeconds);
+        }
+        if (option == NetherChannelOption.NETHER_SERVER_ANSWER_DECORATOR) {
+            return (T) this.answerDecorator;
         }
 
         return super.getOption(option);
@@ -35,6 +41,9 @@ public class DefaultNetherServerChannelConfig extends DefaultNetherChannelConfig
 
         if (option == NetherChannelOption.NETHER_SERVER_RTC_HANDSHAKE_TIMEOUT_SECONDS) {
             this.setServerRtcHandshakeTimeoutSeconds((Integer) value);
+            return true;
+        } else if (option == NetherChannelOption.NETHER_SERVER_ANSWER_DECORATOR) {
+            this.answerDecorator = (NetherNetAnswerDecorator) value;
             return true;
         } else {
             return super.setOption(option, value);

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/config/NetherChannelOption.java
@@ -1,9 +1,18 @@
 package org.cloudburstmc.netty.channel.nethernet.config;
 
+import org.cloudburstmc.netty.channel.nethernet.NetherNetAnswerDecorator;
 import dev.kastle.webrtc.PortAllocatorConfig;
 import io.netty.channel.ChannelOption;
 
 public class NetherChannelOption<T> extends ChannelOption<T> {
+
+    /**
+     * Transforms every SDP answer before it is signaled, e.g. to attach the
+     * server identity assertion. Null (the default) signals answers as
+     * negotiated.
+     */
+    public static final ChannelOption<NetherNetAnswerDecorator> NETHER_SERVER_ANSWER_DECORATOR =
+            valueOf(NetherChannelOption.class, "NETHER_SERVER_ANSWER_DECORATOR");
 
     /**
      * The PortAllocatorConfig used for WebRTC connections.

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/signaling/NetherNetHttpSignalingTest.java
@@ -1,0 +1,148 @@
+package org.cloudburstmc.netty.channel.nethernet.signaling;
+
+import org.cloudburstmc.netty.channel.nethernet.NetherNetServerChannel;
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcServerBackend;
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSession;
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSessionListener;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Exercises the HTTP signaling front end against the real netty stack and
+ * server channel, with the WebRTC engine replaced by a scripted backend: an
+ * accepted offer produces a canned full ICE answer on a separate thread,
+ * mirroring how the engine reports the answer after gathering completes.
+ */
+class NetherNetHttpSignalingTest {
+
+    private static final String CANNED_ANSWER = "v=0\r\no=- 0 2 IN IP4 127.0.0.1\r\na=candidate:1 1 udp 2122260223 127.0.0.1 55555 typ host\r\n";
+
+    private static NioEventLoopGroup group;
+    private static NetherNetHttpSignaling signaling;
+    private static Channel serverChannel;
+    private static ScriptedBackend backend;
+    private static HttpClient client;
+    private static String baseUrl;
+
+    /** Records accepts and answers each offer with the canned SDP. */
+    private static final class ScriptedBackend implements WebRtcServerBackend {
+        final List<Boolean> fullIceFlags = new CopyOnWriteArrayList<>();
+        volatile boolean answerOffers = true;
+
+        @Override
+        public WebRtcSession accept(String offerSdp, List<NetherNetSignaling.IceServerInfo> iceServers,
+                                    WebRtcSessionListener listener, boolean fullIceAnswer) {
+            fullIceFlags.add(fullIceAnswer);
+            if (answerOffers) {
+                // Engine threads report the answer asynchronously; model that.
+                CompletableFuture.runAsync(() -> listener.onAnswerReady(CANNED_ANSWER));
+            }
+            return new WebRtcSession() {
+                @Override
+                public void send(ByteBuffer data) {
+                }
+
+                @Override
+                public void addRemoteCandidate(String candidateSdp) {
+                }
+
+                @Override
+                public void close() {
+                }
+            };
+        }
+
+        @Override
+        public void close() {
+        }
+    }
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        group = new NioEventLoopGroup(2);
+        backend = new ScriptedBackend();
+        signaling = new NetherNetHttpSignaling(null, group);
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(group, group)
+                .channelFactory(() -> new NetherNetServerChannel(backend, signaling))
+                .childHandler(new ChannelInboundHandlerAdapter());
+        serverChannel = bootstrap.bind(new InetSocketAddress("127.0.0.1", 0)).sync().channel();
+
+        InetSocketAddress bound = signaling.boundAddress();
+        baseUrl = "http://127.0.0.1:" + bound.getPort();
+        client = HttpClient.newHttpClient();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (serverChannel != null) {
+            serverChannel.close().syncUninterruptibly();
+        }
+        if (group != null) {
+            group.shutdownGracefully(0, 3, TimeUnit.SECONDS).syncUninterruptibly();
+        }
+    }
+
+    private static HttpResponse<String> get(String path) throws Exception {
+        return client.send(HttpRequest.newBuilder(URI.create(baseUrl + path)).GET().build(),
+                HttpResponse.BodyHandlers.ofString());
+    }
+
+    private static HttpResponse<String> post(String path, String body) throws Exception {
+        return client.send(HttpRequest.newBuilder(URI.create(baseUrl + path))
+                        .POST(HttpRequest.BodyPublishers.ofString(body)).build(),
+                HttpResponse.BodyHandlers.ofString());
+    }
+
+    @Test
+    void joinProbeAnswers200() throws Exception {
+        assertEquals(200, get("/v1/join").statusCode());
+    }
+
+    @Test
+    void unknownPathAnswers404() throws Exception {
+        assertEquals(404, get("/v1/other").statusCode());
+        assertEquals(404, post("/v2/join/123", "x").statusCode());
+    }
+
+    @Test
+    void offerProducesFullIceAnswer() throws Exception {
+        HttpResponse<String> response = post("/v1/join/12345678901234567890", "v=0\r\nfake offer\r\n");
+        assertEquals(200, response.statusCode());
+        assertEquals("application/sdp", response.headers().firstValue("content-type").orElse(""));
+        assertEquals(CANNED_ANSWER, response.body());
+        assertTrue(response.headers().firstValue("connection").orElse("").equalsIgnoreCase("close"));
+        // The channel must have requested a full ICE answer from the backend.
+        assertTrue(backend.fullIceFlags.stream().allMatch(Boolean::booleanValue));
+    }
+
+    @Test
+    void nonNumericNetworkIdAnswers400() throws Exception {
+        assertEquals(400, post("/v1/join/not-a-number", "v=0\r\n").statusCode());
+    }
+
+    @Test
+    void emptyOfferAnswers400() throws Exception {
+        assertEquals(400, post("/v1/join/42", "").statusCode());
+    }
+}