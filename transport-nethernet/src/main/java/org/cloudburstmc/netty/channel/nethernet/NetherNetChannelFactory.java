SendableMetatype/NetworkCompatible

Create server backends lazily so a failed signaling bind creates no native state
NetherNetServerChannel gains a backend supplier constructor: the backend
(and with it the PeerConnectionFactory pool) is created in doBind only
after the signaling endpoint bound successfully. Previously consumers had
to create the pool before channel construction, so a signaling endpoint
that could not bind, such as a taken TCP port on shared hosting, meant
disposing freshly created factories milliseconds after creation. That
teardown races engine initialization inside libwebrtc and aborts the JVM
with a pure virtual call.

Offers racing a failed bind are dropped safely, doClose tolerates a
backend that never materialized, and NetherNetChannelFactory gains a
supplier overload building the LibWebRtcServerBackend on demand. The
eager constructors are unchanged.

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetChannelFactory.java
@@ -1,5 +1,6 @@
 package dev.kastle.netty.channel.nethernet;
 
+import dev.kastle.netty.channel.nethernet.backend.LibWebRtcServerBackend;
 import dev.kastle.netty.channel.nethernet.backend.WebRtcServerBackend;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetClientSignaling;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetServerSignaling;
@@ -49,6 +50,25 @@ public class NetherNetChannelFactory<T extends Channel> implements ChannelFactor
      */
     public static ChannelFactory<NetherNetServerChannel> server(List<PeerConnectionFactory> factories, NetherNetServerSignaling signaling) {
         return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(factories, signaling));
+    }
+
+    /**
+     * Creates a NetherNet Server Channel Factory whose PeerConnectionFactory
+     * pool is created lazily, only after the channel's signaling endpoint
+     * bound successfully. A bind that fails (a taken TCP port, a refused
+     * websocket) therefore never creates native engine state; there is
+     * nothing to tear down on that path.
+     *
+     * @param factoriesSupplier Invoked once per channel after its signaling
+     *                          bind succeeded; must return a non empty pool.
+     *                          The channel takes ownership and disposes every
+     *                          factory on close.
+     * @param signaling         The NetherNetServerSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetServerChannel.
+     */
+    public static ChannelFactory<NetherNetServerChannel> server(Supplier<List<PeerConnectionFactory>> factoriesSupplier, NetherNetServerSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(
+                () -> new LibWebRtcServerBackend(factoriesSupplier.get()), signaling));
     }
 
     /**

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/NetherNetServerChannelLazyBackendTest.java
@@ -1,0 +1,153 @@
+package dev.kastle.netty.channel.nethernet;
+
+import dev.kastle.netty.channel.nethernet.backend.WebRtcServerBackend;
+import dev.kastle.netty.channel.nethernet.backend.WebRtcSession;
+import dev.kastle.netty.channel.nethernet.backend.WebRtcSessionListener;
+import dev.kastle.netty.channel.nethernet.signaling.NetherNetServerSignaling;
+import dev.kastle.netty.channel.nethernet.signaling.NetherNetSignaling.IceServerInfo;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Locks the lazy backend contract: a signaling endpoint that fails to bind
+ * must never cause backend (native engine) creation, and a successful bind
+ * materializes the backend exactly once. This is the regression guard for
+ * the create-then-immediately-dispose churn that a taken TCP port used to
+ * trigger on shared hosts.
+ */
+class NetherNetServerChannelLazyBackendTest {
+
+    private final NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+    @AfterEach
+    void tearDown() {
+        group.shutdownGracefully(0, 1, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void failedSignalingBindNeverCreatesBackend() throws Exception {
+        AtomicInteger backendCreations = new AtomicInteger();
+        StubSignaling signaling = new StubSignaling(true);
+
+        ChannelFuture future = bootstrap(() -> {
+            backendCreations.incrementAndGet();
+            return new StubBackend();
+        }, signaling).bind(new InetSocketAddress(0));
+
+        assertThrows(Exception.class, future::sync);
+        assertEquals(0, backendCreations.get(), "backend must not be created when the signaling bind fails");
+
+        // Closing the failed channel must not throw despite no backend existing.
+        future.channel().close().syncUninterruptibly();
+        assertTrue(signaling.closed, "doClose still closes the signaling endpoint");
+    }
+
+    @Test
+    void successfulBindCreatesBackendOnce() throws Exception {
+        AtomicInteger backendCreations = new AtomicInteger();
+        StubBackend backend = new StubBackend();
+        StubSignaling signaling = new StubSignaling(false);
+
+        ChannelFuture future = bootstrap(() -> {
+            backendCreations.incrementAndGet();
+            return backend;
+        }, signaling).bind(new InetSocketAddress(0));
+
+        future.sync();
+        assertTrue(signaling.bound, "signaling bound before the backend materialized");
+        assertEquals(1, backendCreations.get(), "backend materializes exactly once per bound channel");
+        assertFalse(backend.closed);
+
+        future.channel().close().syncUninterruptibly();
+        assertTrue(backend.closed, "channel close disposes the materialized backend");
+        assertTrue(signaling.closed);
+    }
+
+    private ServerBootstrap bootstrap(java.util.function.Supplier<WebRtcServerBackend> backendSupplier,
+                                      NetherNetServerSignaling signaling) {
+        return new ServerBootstrap()
+                .group(group)
+                .channelFactory(() -> new NetherNetServerChannel(backendSupplier, signaling))
+                .childHandler(new ChannelInboundHandlerAdapter());
+    }
+
+    private static final class StubSignaling implements NetherNetServerSignaling {
+        private final boolean failBind;
+        volatile boolean bound;
+        volatile boolean closed;
+
+        private StubSignaling(boolean failBind) {
+            this.failBind = failBind;
+        }
+
+        @Override
+        public void bind(SocketAddress localAddress) throws ConnectException {
+            if (failBind) {
+                throw new ConnectException("port taken");
+            }
+            bound = true;
+        }
+
+        @Override
+        public void setNewConnectionHandler(NewConnectionHandler handler) {
+        }
+
+        @Override
+        public void setAdvertisementData(PongData pongData) {
+        }
+
+        @Override
+        public void sendSignal(String targetNetworkId, String data) {
+        }
+
+        @Override
+        public void setSignalHandler(long connectionId, SignalHandler handler) {
+        }
+
+        @Override
+        public void removeSignalHandler(long connectionId) {
+        }
+
+        @Override
+        public String getLocalNetworkId() {
+            return "0";
+        }
+
+        @Override
+        public void close() {
+            closed = true;
+        }
+    }
+
+    private static final class StubBackend implements WebRtcServerBackend {
+        volatile boolean closed;
+
+        @Override
+        public WebRtcSession accept(String offerSdp, List<IceServerInfo> iceServers,
+                                    WebRtcSessionListener listener, boolean fullIceAnswer) {
+            throw new UnsupportedOperationException("not under test");
+        }
+
+        @Override
+        public void close() {
+            closed = true;
+        }
+    }
+}

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChannelFactory.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.netty.channel.nethernet;
 
+import org.cloudburstmc.netty.channel.nethernet.backend.LibWebRtcServerBackend;
 import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcServerBackend;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetClientSignaling;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetServerSignaling;
@@ -49,6 +50,25 @@ public class NetherNetChannelFactory<T extends Channel> implements ChannelFactor
      */
     public static ChannelFactory<NetherNetServerChannel> server(List<PeerConnectionFactory> factories, NetherNetServerSignaling signaling) {
         return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(factories, signaling));
+    }
+
+    /**
+     * Creates a NetherNet Server Channel Factory whose PeerConnectionFactory
+     * pool is created lazily, only after the channel's signaling endpoint
+     * bound successfully. A bind that fails (a taken TCP port, a refused
+     * websocket) therefore never creates native engine state; there is
+     * nothing to tear down on that path.
+     *
+     * @param factoriesSupplier Invoked once per channel after its signaling
+     *                          bind succeeded; must return a non empty pool.
+     *                          The channel takes ownership and disposes every
+     *                          factory on close.
+     * @param signaling         The NetherNetServerSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetServerChannel.
+     */
+    public static ChannelFactory<NetherNetServerChannel> server(Supplier<List<PeerConnectionFactory>> factoriesSupplier, NetherNetServerSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(
+                () -> new LibWebRtcServerBackend(factoriesSupplier.get()), signaling));
     }
 
     /**

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/NetherNetServerChannelLazyBackendTest.java
@@ -1,0 +1,153 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcServerBackend;
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSession;
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSessionListener;
+import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetServerSignaling;
+import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetSignaling.IceServerInfo;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Locks the lazy backend contract: a signaling endpoint that fails to bind
+ * must never cause backend (native engine) creation, and a successful bind
+ * materializes the backend exactly once. This is the regression guard for
+ * the create-then-immediately-dispose churn that a taken TCP port used to
+ * trigger on shared hosts.
+ */
+class NetherNetServerChannelLazyBackendTest {
+
+    private final NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+    @AfterEach
+    void tearDown() {
+        group.shutdownGracefully(0, 1, TimeUnit.SECONDS);
+    }
+
+    @Test
+    void failedSignalingBindNeverCreatesBackend() throws Exception {
+        AtomicInteger backendCreations = new AtomicInteger();
+        StubSignaling signaling = new StubSignaling(true);
+
+        ChannelFuture future = bootstrap(() -> {
+            backendCreations.incrementAndGet();
+            return new StubBackend();
+        }, signaling).bind(new InetSocketAddress(0));
+
+        assertThrows(Exception.class, future::sync);
+        assertEquals(0, backendCreations.get(), "backend must not be created when the signaling bind fails");
+
+        // Closing the failed channel must not throw despite no backend existing.
+        future.channel().close().syncUninterruptibly();
+        assertTrue(signaling.closed, "doClose still closes the signaling endpoint");
+    }
+
+    @Test
+    void successfulBindCreatesBackendOnce() throws Exception {
+        AtomicInteger backendCreations = new AtomicInteger();
+        StubBackend backend = new StubBackend();
+        StubSignaling signaling = new StubSignaling(false);
+
+        ChannelFuture future = bootstrap(() -> {
+            backendCreations.incrementAndGet();
+            return backend;
+        }, signaling).bind(new InetSocketAddress(0));
+
+        future.sync();
+        assertTrue(signaling.bound, "signaling bound before the backend materialized");
+        assertEquals(1, backendCreations.get(), "backend materializes exactly once per bound channel");
+        assertFalse(backend.closed);
+
+        future.channel().close().syncUninterruptibly();
+        assertTrue(backend.closed, "channel close disposes the materialized backend");
+        assertTrue(signaling.closed);
+    }
+
+    private ServerBootstrap bootstrap(java.util.function.Supplier<WebRtcServerBackend> backendSupplier,
+                                      NetherNetServerSignaling signaling) {
+        return new ServerBootstrap()
+                .group(group)
+                .channelFactory(() -> new NetherNetServerChannel(backendSupplier, signaling))
+                .childHandler(new ChannelInboundHandlerAdapter());
+    }
+
+    private static final class StubSignaling implements NetherNetServerSignaling {
+        private final boolean failBind;
+        volatile boolean bound;
+        volatile boolean closed;
+
+        private StubSignaling(boolean failBind) {
+            this.failBind = failBind;
+        }
+
+        @Override
+        public void bind(SocketAddress localAddress) throws ConnectException {
+            if (failBind) {
+                throw new ConnectException("port taken");
+            }
+            bound = true;
+        }
+
+        @Override
+        public void setNewConnectionHandler(NewConnectionHandler handler) {
+        }
+
+        @Override
+        public void setAdvertisementData(PongData pongData) {
+        }
+
+        @Override
+        public void sendSignal(String targetNetworkId, String data) {
+        }
+
+        @Override
+        public void setSignalHandler(long connectionId, SignalHandler handler) {
+        }
+
+        @Override
+        public void removeSignalHandler(long connectionId) {
+        }
+
+        @Override
+        public String getLocalNetworkId() {
+            return "0";
+        }
+
+        @Override
+        public void close() {
+            closed = true;
+        }
+    }
+
+    private static final class StubBackend implements WebRtcServerBackend {
+        volatile boolean closed;
+
+        @Override
+        public WebRtcSession accept(String offerSdp, List<IceServerInfo> iceServers,
+                                    WebRtcSessionListener listener, boolean fullIceAnswer) {
+            throw new UnsupportedOperationException("not under test");
+        }
+
+        @Override
+        public void close() {
+            closed = true;
+        }
+    }
+}