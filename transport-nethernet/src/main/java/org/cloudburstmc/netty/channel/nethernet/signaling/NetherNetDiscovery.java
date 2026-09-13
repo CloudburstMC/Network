EduGeyser/NetworkM

Upgrade NetherNet LAN advertisements to the stable v6 format

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/signaling/NetherNetDiscovery.java
@@ -1,10 +1,12 @@
 package dev.kastle.netty.channel.nethernet.signaling;
 
 import dev.kastle.netty.channel.nethernet.NetherNetConstants;
+import dev.kastle.netty.channel.nethernet.codec.NetherNetServerDataCodec;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetServerSignaling.PongData;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetSignaling.SignalHandler;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
@@ -23,7 +25,6 @@ import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.HexFormat;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -34,6 +35,8 @@ import java.util.function.Supplier;
 
 public class NetherNetDiscovery extends SimpleChannelInboundHandler<DatagramPacket> {
     private static final InternalLogger log = InternalLoggerFactory.getInstance(NetherNetDiscovery.class);
+    // Hex expansion, 24 plaintext header bytes, a 32-byte MAC and AES padding must fit a 65,507-byte UDP payload.
+    private static final int MAX_SERVER_DATA_BYTES = 32_723;
 
     private final long networkId;
     private final Map<Long, SignalHandler> signalHandlers = new ConcurrentHashMap<>();
@@ -170,27 +173,17 @@ public class NetherNetDiscovery extends SimpleChannelInboundHandler<DatagramPack
     private record DiscoveryCallback(BiConsumer<Long, ByteBuf> consumer, InetSocketAddress expectedSender) { }
 
     public void setPongData(PongData data) {
-        ByteBuf buf = Unpooled.buffer();
-        byte[] binaryData;
+        Objects.requireNonNull(data, "data");
+        ByteBuf buf = Unpooled.buffer(128, MAX_SERVER_DATA_BYTES);
+        byte[] hexBytes;
         try {
-            buf.writeByte(4); // Version
-            writeString(buf, data.serverName());
-            writeString(buf, data.levelName());
-            writeSignedVarInt(buf, data.gameType());
-            buf.writeIntLE(data.playerCount());
-            buf.writeIntLE(data.maxPlayerCount());
-            buf.writeBoolean(data.isEditorWorld());
-            buf.writeBoolean(data.isHardcore());
-            writeSignedVarInt(buf, data.transportLayer());
-            writeSignedVarInt(buf, data.connectionType());
-            binaryData = new byte[buf.readableBytes()];
-            buf.readBytes(binaryData);
+            NetherNetServerDataCodec.encode(buf, data);
+            hexBytes = ByteBufUtil.hexDump(buf).getBytes(StandardCharsets.US_ASCII);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("LAN advertisement exceeds the UDP payload limit", e);
         } finally {
             buf.release();
         }
-
-        String hex = HexFormat.of().formatHex(binaryData);
-        byte[] hexBytes = hex.getBytes(StandardCharsets.UTF_8);
 
         ByteBuf response = Unpooled.buffer();
         try {
@@ -491,23 +484,5 @@ public class NetherNetDiscovery extends SimpleChannelInboundHandler<DatagramPack
     public boolean isActive() {
         Channel channel = this.channel;
         return !closed && channel != null && channel.isActive();
-    }
-
-    private void writeString(ByteBuf buf, String s) {
-        byte[] b = s.getBytes(StandardCharsets.UTF_8);
-        this.writeUnsignedVarInt(buf, b.length);
-        buf.writeBytes(b);
-    }
-
-    private void writeUnsignedVarInt(ByteBuf buf, int value) {
-        while ((value & 0xFFFFFF80) != 0) {
-            buf.writeByte((byte) ((value & 0x7F) | 0x80));
-            value >>>= 7;
-        }
-        buf.writeByte((byte) value);
-    }
-
-    private void writeSignedVarInt(ByteBuf buf, int value) {
-        writeUnsignedVarInt(buf, (value << 1) ^ (value >> 31));
     }
 }

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/signaling/NetherNetServerSignaling.java
@@ -1,9 +1,12 @@
 package dev.kastle.netty.channel.nethernet.signaling;
 
+import dev.kastle.netty.channel.nethernet.NetherNetServerStatus;
+
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.List;
+import java.util.Objects;
 
 public interface NetherNetServerSignaling extends NetherNetSignaling {
     /**
@@ -73,7 +76,8 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
     }
 
     /**
-     * Data structure for Pong advertisement data.
+     * LAN v6 advertisement fields. Authentication flags describe the consumer's
+     * admission policy; setting them does not enable authentication or nonce checks.
      *
      * @param serverName      The name of the server.
      * @param levelName       The name of the level/world.
@@ -84,9 +88,31 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
      * @param isHardcore      Whether the world is in hardcore mode.
      * @param transportLayer  The transport layer identifier (e.g. NetherNet).
      * @param connectionType  The connection type identifier (e.g. LAN, Online).
+     * @param acceptsOnlineAuth whether online-authenticated players may join
+     * @param acceptsSelfSignedAuth whether self-signed identities may join
+     * @param nonce the host's nonce, which clients echo in their Login data
      */
     public record PongData(String serverName, String levelName, int gameType, int playerCount, int maxPlayerCount,
-            boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType) {
+            boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType,
+            boolean acceptsOnlineAuth, boolean acceptsSelfSignedAuth, String nonce) {
+        private static final String DEFAULT_NONCE = NetherNetServerStatus.randomNonce();
+
+        public PongData {
+            Objects.requireNonNull(serverName, "serverName");
+            Objects.requireNonNull(levelName, "levelName");
+            Objects.requireNonNull(nonce, "nonce");
+        }
+
+        /**
+         * Preserves the original constructor. Both identity types are advertised
+         * as accepted, and the default nonce is generated once per process.
+         */
+        public PongData(String serverName, String levelName, int gameType, int playerCount, int maxPlayerCount,
+                        boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType) {
+            this(serverName, levelName, gameType, playerCount, maxPlayerCount, isEditorWorld, isHardcore,
+                    transportLayer, connectionType, true, true, DEFAULT_NONCE);
+        }
+
         public static class Builder {
             private String serverName = "Server";
             private String levelName = "World";
@@ -97,6 +123,9 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
             private boolean isHardcore = false;
             private int transportLayer = 2; // Default to NetherNet
             private int connectionType = 4; // Default to LAN
+            private boolean acceptsOnlineAuth = true;
+            private boolean acceptsSelfSignedAuth = true;
+            private String nonce = DEFAULT_NONCE;
 
             public Builder setServerName(String serverName) {
                 this.serverName = serverName;
@@ -143,9 +172,26 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
                 return this;
             }
 
+            public Builder setAcceptsOnlineAuth(boolean acceptsOnlineAuth) {
+                this.acceptsOnlineAuth = acceptsOnlineAuth;
+                return this;
+            }
+
+            public Builder setAcceptsSelfSignedAuth(boolean acceptsSelfSignedAuth) {
+                this.acceptsSelfSignedAuth = acceptsSelfSignedAuth;
+                return this;
+            }
+
+            /** Supply the consumer's shared nonce when advertising through multiple endpoints. */
+            public Builder setNonce(String nonce) {
+                this.nonce = Objects.requireNonNull(nonce, "nonce");
+                return this;
+            }
+
             public PongData build() {
                 return new PongData(serverName, levelName, gameType, playerCount, maxPlayerCount,
-                    isEditorWorld, isHardcore, transportLayer, connectionType);
+                    isEditorWorld, isHardcore, transportLayer, connectionType,
+                    acceptsOnlineAuth, acceptsSelfSignedAuth, nonce);
             }
         }
     }

transport-nethernet/src/main/java/dev/kastle/netty/util/nethernet/NetherNetScanner.java
@@ -1,6 +1,7 @@
 package dev.kastle.netty.util.nethernet;
 
 import dev.kastle.netty.channel.nethernet.NetherNetConstants;
+import dev.kastle.netty.channel.nethernet.codec.NetherNetServerDataCodec;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetDiscovery;
 import dev.kastle.netty.channel.nethernet.signaling.NetherNetServerSignaling.PongData;
 import io.netty.buffer.ByteBuf;
@@ -37,6 +38,9 @@ public class NetherNetScanner {
                 System.out.println("Game Mode: " + data.gameType());
                 System.out.println("Editor World: " + data.isEditorWorld());
                 System.out.println("Hardcore: " + data.isHardcore());
+                System.out.println("Accepts Online Auth: " + data.acceptsOnlineAuth());
+                System.out.println("Accepts Self-Signed Auth: " + data.acceptsSelfSignedAuth());
+                System.out.println("Nonce: " + data.nonce());
                 System.out.println("Version: " + info.version());
                 System.out.println("--------------------------------");
             } catch (Exception e) {
@@ -61,45 +65,10 @@ public class NetherNetScanner {
         String hex = payload.readCharSequence(length, StandardCharsets.US_ASCII).toString();
         ByteBuf data = Unpooled.wrappedBuffer(ByteBufUtil.decodeHexDump(hex));
         try {
-            int version = data.readUnsignedByte();
-            PongData pong = new PongData(readString(data), readString(data), readSignedVarInt(data),
-                    data.readIntLE(), data.readIntLE(), data.readBoolean(), data.readBoolean(),
-                    readSignedVarInt(data), readSignedVarInt(data));
-            return new ServerInfo(version, pong);
+            return new ServerInfo(NetherNetServerDataCodec.VERSION, NetherNetServerDataCodec.decode(data));
         } finally {
             data.release();
         }
-    }
-
-    private static String readString(ByteBuf buf) {
-        int len = readUnsignedVarInt(buf);
-        if (len < 0 || buf.readableBytes() < len) {
-            throw new IllegalArgumentException("Invalid discovery string length");
-        }
-        return buf.readCharSequence(len, StandardCharsets.UTF_8).toString();
-    }
-
-    private static int readSignedVarInt(ByteBuf buf) {
-        int encoded = readUnsignedVarInt(buf);
-        return (encoded >>> 1) ^ -(encoded & 1);
-    }
-
-    private static int readUnsignedVarInt(ByteBuf buf) {
-        int value = 0;
-        for (int shift = 0; shift <= 28; shift += 7) {
-            if (!buf.isReadable()) {
-                throw new IllegalArgumentException("Truncated discovery varint");
-            }
-            int next = buf.readUnsignedByte();
-            if (shift == 28 && (next & 0xf0) != 0) {
-                throw new IllegalArgumentException("Discovery varint exceeds 32 bits");
-            }
-            value |= (next & 0x7f) << shift;
-            if ((next & 0x80) == 0) {
-                return value;
-            }
-        }
-        throw new IllegalArgumentException("Discovery varint exceeds 32 bits");
     }
 
     record ServerInfo(int version, PongData data) { }

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/signaling/NetherNetDiscovery.java
@@ -1,10 +1,12 @@
 package org.cloudburstmc.netty.channel.nethernet.signaling;
 
 import org.cloudburstmc.netty.channel.nethernet.NetherNetConstants;
+import org.cloudburstmc.netty.channel.nethernet.codec.NetherNetServerDataCodec;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetServerSignaling.PongData;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetSignaling.SignalHandler;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
@@ -23,7 +25,6 @@ import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.HexFormat;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
@@ -34,6 +35,8 @@ import java.util.function.Supplier;
 
 public class NetherNetDiscovery extends SimpleChannelInboundHandler<DatagramPacket> {
     private static final InternalLogger log = InternalLoggerFactory.getInstance(NetherNetDiscovery.class);
+    // Hex expansion, 24 plaintext header bytes, a 32-byte MAC and AES padding must fit a 65,507-byte UDP payload.
+    private static final int MAX_SERVER_DATA_BYTES = 32_723;
 
     private final long networkId;
     private final Map<Long, SignalHandler> signalHandlers = new ConcurrentHashMap<>();
@@ -170,27 +173,17 @@ public class NetherNetDiscovery extends SimpleChannelInboundHandler<DatagramPack
     private record DiscoveryCallback(BiConsumer<Long, ByteBuf> consumer, InetSocketAddress expectedSender) { }
 
     public void setPongData(PongData data) {
-        ByteBuf buf = Unpooled.buffer();
-        byte[] binaryData;
+        Objects.requireNonNull(data, "data");
+        ByteBuf buf = Unpooled.buffer(128, MAX_SERVER_DATA_BYTES);
+        byte[] hexBytes;
         try {
-            buf.writeByte(4); // Version
-            writeString(buf, data.serverName());
-            writeString(buf, data.levelName());
-            writeSignedVarInt(buf, data.gameType());
-            buf.writeIntLE(data.playerCount());
-            buf.writeIntLE(data.maxPlayerCount());
-            buf.writeBoolean(data.isEditorWorld());
-            buf.writeBoolean(data.isHardcore());
-            writeSignedVarInt(buf, data.transportLayer());
-            writeSignedVarInt(buf, data.connectionType());
-            binaryData = new byte[buf.readableBytes()];
-            buf.readBytes(binaryData);
+            NetherNetServerDataCodec.encode(buf, data);
+            hexBytes = ByteBufUtil.hexDump(buf).getBytes(StandardCharsets.US_ASCII);
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("LAN advertisement exceeds the UDP payload limit", e);
         } finally {
             buf.release();
         }
-
-        String hex = HexFormat.of().formatHex(binaryData);
-        byte[] hexBytes = hex.getBytes(StandardCharsets.UTF_8);
 
         ByteBuf response = Unpooled.buffer();
         try {
@@ -491,23 +484,5 @@ public class NetherNetDiscovery extends SimpleChannelInboundHandler<DatagramPack
     public boolean isActive() {
         Channel channel = this.channel;
         return !closed && channel != null && channel.isActive();
-    }
-
-    private void writeString(ByteBuf buf, String s) {
-        byte[] b = s.getBytes(StandardCharsets.UTF_8);
-        this.writeUnsignedVarInt(buf, b.length);
-        buf.writeBytes(b);
-    }
-
-    private void writeUnsignedVarInt(ByteBuf buf, int value) {
-        while ((value & 0xFFFFFF80) != 0) {
-            buf.writeByte((byte) ((value & 0x7F) | 0x80));
-            value >>>= 7;
-        }
-        buf.writeByte((byte) value);
-    }
-
-    private void writeSignedVarInt(ByteBuf buf, int value) {
-        writeUnsignedVarInt(buf, (value << 1) ^ (value >> 31));
     }
 }

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/signaling/NetherNetServerSignaling.java
@@ -1,9 +1,12 @@
 package org.cloudburstmc.netty.channel.nethernet.signaling;
 
+import org.cloudburstmc.netty.channel.nethernet.NetherNetServerStatus;
+
 import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.List;
+import java.util.Objects;
 
 public interface NetherNetServerSignaling extends NetherNetSignaling {
     /**
@@ -73,7 +76,8 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
     }
 
     /**
-     * Data structure for Pong advertisement data.
+     * LAN v6 advertisement fields. Authentication flags describe the consumer's
+     * admission policy; setting them does not enable authentication or nonce checks.
      *
      * @param serverName      The name of the server.
      * @param levelName       The name of the level/world.
@@ -84,9 +88,31 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
      * @param isHardcore      Whether the world is in hardcore mode.
      * @param transportLayer  The transport layer identifier (e.g. NetherNet).
      * @param connectionType  The connection type identifier (e.g. LAN, Online).
+     * @param acceptsOnlineAuth whether online-authenticated players may join
+     * @param acceptsSelfSignedAuth whether self-signed identities may join
+     * @param nonce the host's nonce, which clients echo in their Login data
      */
     public record PongData(String serverName, String levelName, int gameType, int playerCount, int maxPlayerCount,
-            boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType) {
+            boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType,
+            boolean acceptsOnlineAuth, boolean acceptsSelfSignedAuth, String nonce) {
+        private static final String DEFAULT_NONCE = NetherNetServerStatus.randomNonce();
+
+        public PongData {
+            Objects.requireNonNull(serverName, "serverName");
+            Objects.requireNonNull(levelName, "levelName");
+            Objects.requireNonNull(nonce, "nonce");
+        }
+
+        /**
+         * Preserves the original constructor. Both identity types are advertised
+         * as accepted, and the default nonce is generated once per process.
+         */
+        public PongData(String serverName, String levelName, int gameType, int playerCount, int maxPlayerCount,
+                        boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType) {
+            this(serverName, levelName, gameType, playerCount, maxPlayerCount, isEditorWorld, isHardcore,
+                    transportLayer, connectionType, true, true, DEFAULT_NONCE);
+        }
+
         public static class Builder {
             private String serverName = "Server";
             private String levelName = "World";
@@ -97,6 +123,9 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
             private boolean isHardcore = false;
             private int transportLayer = 2; // Default to NetherNet
             private int connectionType = 4; // Default to LAN
+            private boolean acceptsOnlineAuth = true;
+            private boolean acceptsSelfSignedAuth = true;
+            private String nonce = DEFAULT_NONCE;
 
             public Builder setServerName(String serverName) {
                 this.serverName = serverName;
@@ -143,9 +172,26 @@ public interface NetherNetServerSignaling extends NetherNetSignaling {
                 return this;
             }
 
+            public Builder setAcceptsOnlineAuth(boolean acceptsOnlineAuth) {
+                this.acceptsOnlineAuth = acceptsOnlineAuth;
+                return this;
+            }
+
+            public Builder setAcceptsSelfSignedAuth(boolean acceptsSelfSignedAuth) {
+                this.acceptsSelfSignedAuth = acceptsSelfSignedAuth;
+                return this;
+            }
+
+            /** Supply the consumer's shared nonce when advertising through multiple endpoints. */
+            public Builder setNonce(String nonce) {
+                this.nonce = Objects.requireNonNull(nonce, "nonce");
+                return this;
+            }
+
             public PongData build() {
                 return new PongData(serverName, levelName, gameType, playerCount, maxPlayerCount,
-                    isEditorWorld, isHardcore, transportLayer, connectionType);
+                    isEditorWorld, isHardcore, transportLayer, connectionType,
+                    acceptsOnlineAuth, acceptsSelfSignedAuth, nonce);
             }
         }
     }

transport-nethernet/src/main/java/org/cloudburstmc/netty/util/nethernet/NetherNetScanner.java
@@ -1,6 +1,7 @@
 package org.cloudburstmc.netty.util.nethernet;
 
 import org.cloudburstmc.netty.channel.nethernet.NetherNetConstants;
+import org.cloudburstmc.netty.channel.nethernet.codec.NetherNetServerDataCodec;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetDiscovery;
 import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetServerSignaling.PongData;
 import io.netty.buffer.ByteBuf;
@@ -37,6 +38,9 @@ public class NetherNetScanner {
                 System.out.println("Game Mode: " + data.gameType());
                 System.out.println("Editor World: " + data.isEditorWorld());
                 System.out.println("Hardcore: " + data.isHardcore());
+                System.out.println("Accepts Online Auth: " + data.acceptsOnlineAuth());
+                System.out.println("Accepts Self-Signed Auth: " + data.acceptsSelfSignedAuth());
+                System.out.println("Nonce: " + data.nonce());
                 System.out.println("Version: " + info.version());
                 System.out.println("--------------------------------");
             } catch (Exception e) {
@@ -61,45 +65,10 @@ public class NetherNetScanner {
         String hex = payload.readCharSequence(length, StandardCharsets.US_ASCII).toString();
         ByteBuf data = Unpooled.wrappedBuffer(ByteBufUtil.decodeHexDump(hex));
         try {
-            int version = data.readUnsignedByte();
-            PongData pong = new PongData(readString(data), readString(data), readSignedVarInt(data),
-                    data.readIntLE(), data.readIntLE(), data.readBoolean(), data.readBoolean(),
-                    readSignedVarInt(data), readSignedVarInt(data));
-            return new ServerInfo(version, pong);
+            return new ServerInfo(NetherNetServerDataCodec.VERSION, NetherNetServerDataCodec.decode(data));
         } finally {
             data.release();
         }
-    }
-
-    private static String readString(ByteBuf buf) {
-        int len = readUnsignedVarInt(buf);
-        if (len < 0 || buf.readableBytes() < len) {
-            throw new IllegalArgumentException("Invalid discovery string length");
-        }
-        return buf.readCharSequence(len, StandardCharsets.UTF_8).toString();
-    }
-
-    private static int readSignedVarInt(ByteBuf buf) {
-        int encoded = readUnsignedVarInt(buf);
-        return (encoded >>> 1) ^ -(encoded & 1);
-    }
-
-    private static int readUnsignedVarInt(ByteBuf buf) {
-        int value = 0;
-        for (int shift = 0; shift <= 28; shift += 7) {
-            if (!buf.isReadable()) {
-                throw new IllegalArgumentException("Truncated discovery varint");
-            }
-            int next = buf.readUnsignedByte();
-            if (shift == 28 && (next & 0xf0) != 0) {
-                throw new IllegalArgumentException("Discovery varint exceeds 32 bits");
-            }
-            value |= (next & 0x7f) << shift;
-            if ((next & 0x80) == 0) {
-                return value;
-            }
-        }
-        throw new IllegalArgumentException("Discovery varint exceeds 32 bits");
     }
 
     record ServerInfo(int version, PongData data) { }