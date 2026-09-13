SendableMetatype/NetworkCompatible

Answer the join probe with the server status document
Vanilla 1.26.50 turned GET /v1/join from a pure capability check into the
NetherNet equivalent of RakNet's unconnected pong. The schema has
fourteen members, of which vanilla emits seven, the rest being populated
and then dropped by the serialization context its document selects.

NetherNetServerStatus models all fourteen with every member optional and
independent: setting one emits it, leaving it unset omits it, and members
are emitted in schema registration order. Populating exactly the seven
vanilla sends therefore reproduces its response byte identically, without
this type treating those seven as privileged. That split was observed on
the first server version to answer the endpoint at all, before any client
read the payload, so it is not a contract to encode. Optionality also
keeps an explicit false distinguishable from an absent flag.

The supplier is consulted per request so live counts need no
republishing, and the endpoint keeps its historical empty body when no
supplier is set or when one fails, since the probe decides whether the
client attempts NetherNet at all.

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/signaling/NetherNetHttpSignaling.java
@@ -1,6 +1,7 @@
 package dev.kastle.netty.channel.nethernet.signaling;
 
 import dev.kastle.netty.channel.nethernet.NetherNetConstants;
+import dev.kastle.netty.channel.nethernet.NetherNetServerStatus;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
@@ -48,7 +49,10 @@ import java.util.function.Supplier;
  *
  * Endpoints:
  * - GET /v1/join: any 2xx means NetherNet is supported and the client
- *   proceeds with the SDP exchange.
+ *   proceeds with the SDP exchange. Carries the
+ *   {@link NetherNetServerStatus} document when a supplier is set, which
+ *   vanilla 1.26.50 and later answer with as their equivalent of the RakNet
+ *   unconnected pong; older clients ignore the body.
  * - POST /v1/join/{networkId}: the request body is the client's SDP offer;
  *   the response body is the full ICE SDP answer (application/sdp). The
  *   whole exchange fits one round trip, so {@link #fullIceAnswers()} is true
@@ -80,6 +84,9 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
     private final Map<Long, SignalHandler> signalHandlers = new ConcurrentHashMap<>();
     private final Map<Long, PendingExchange> pendingExchanges = new ConcurrentHashMap<>();
 
+    private volatile Supplier<NetherNetServerStatus> statusSupplier;
+    // Racing a duplicate first warn is harmless; this only bounds the spam.
+    private volatile boolean statusSupplierWarned;
     private volatile NewConnectionHandler newConnectionHandler;
     private volatile Channel serverChannel;
     // The TCP accept loop. Owned: bind() is called from the NetherNet server
@@ -110,6 +117,21 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
     public NetherNetHttpSignaling(Supplier<SslContext> sslContextSupplier, EventLoopGroup workerGroup) {
         this.sslContextSupplier = sslContextSupplier;
         this.workerGroup = workerGroup;
+    }
+
+    /**
+     * Sets the source of the server status answered on the capability check,
+     * or null to answer with an empty body as before. Consulted per request,
+     * so live values (the player count above all) need no republishing; the
+     * consumer owns any caching, since this endpoint is reachable by anyone
+     * who can open a TCP connection.
+     *
+     * Called from connection I/O threads; implementations must be thread
+     * safe and return promptly. A failure is treated as no status being
+     * available and never turns the capability check negative.
+     */
+    public void setStatusSupplier(Supplier<NetherNetServerStatus> statusSupplier) {
+        this.statusSupplier = statusSupplier;
     }
 
     @Override
@@ -293,7 +315,7 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
                 if (newConnectionHandler == null || closed) {
                     respond(ctx, HttpResponseStatus.SERVICE_UNAVAILABLE, "text/plain", "Service unavailable");
                 } else {
-                    respond(ctx, HttpResponseStatus.OK, "text/plain", "");
+                    respondStatus(ctx);
                 }
                 return;
             }
@@ -372,6 +394,46 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
      * after the write, matching the one request per connection model. Safe
      * from any thread.
      */
+    /**
+     * Answers the capability check, carrying the server status document when
+     * the consumer supplies one. A supplier that is absent or that fails
+     * leaves the historical empty body, which every client accepts: the guide
+     * defines the body as ignored, and only 1.26.50 and later read it.
+     */
+    private void respondStatus(ChannelHandlerContext ctx) {
+        Supplier<NetherNetServerStatus> supplier = this.statusSupplier;
+        if (supplier == null) {
+            respond(ctx, HttpResponseStatus.OK, "text/plain", "");
+            return;
+        }
+        String body;
+        try {
+            NetherNetServerStatus status = supplier.get();
+            body = status == null ? null : status.toJson();
+        } catch (Exception e) {
+            // The probe decides whether the client attempts NetherNet at all,
+            // so it must still answer 2xx when the consumer cannot describe
+            // itself. Older clients ignore the body outright; what a reading
+            // client makes of an absent one is not established.
+            //
+            // Warn once, then debug: the endpoint is unauthenticated TCP, so
+            // per request warns would let anyone drive log volume by
+            // hammering the probe against a broken supplier.
+            if (statusSupplierWarned) {
+                log.debug("Server status supplier failed: {}", e.getMessage());
+            } else {
+                statusSupplierWarned = true;
+                log.warn("Server status supplier failed (further failures log at debug): {}", e.getMessage());
+            }
+            body = null;
+        }
+        if (body == null) {
+            respond(ctx, HttpResponseStatus.OK, "text/plain", "");
+        } else {
+            respond(ctx, HttpResponseStatus.OK, "application/json", body);
+        }
+    }
+
     private static void respond(ChannelHandlerContext ctx, HttpResponseStatus status, String contentType, String body) {
         FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status,
                 Unpooled.copiedBuffer(body, StandardCharsets.UTF_8));

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/NetherNetServerStatusTest.java
@@ -1,0 +1,97 @@
+package dev.kastle.netty.channel.nethernet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Locks the wire form of the server status document against the shapes
+ * observed from vanilla 1.26.50.
+ */
+class NetherNetServerStatusTest {
+
+    /** Exactly what a vanilla dedicated server answers, key order included. */
+    private static final String VANILLA_PUBLIC = "{\"name\":\"Dedicated Server\",\"protocol\":2181,"
+            + "\"version\":\"1.26.50\",\"level\":\"Bedrock level\",\"players\":0,\"maxPlayers\":10,\"gameType\":0}";
+
+    /** The full document, recovered from vanilla by widening the context mask. */
+    private static final String VANILLA_FULL = "{\"dataVersion\":7,\"name\":\"Dedicated Server\",\"protocol\":2181,"
+            + "\"version\":\"1.26.50\",\"level\":\"Bedrock level\",\"players\":0,\"maxPlayers\":10,\"gameType\":0,"
+            + "\"editor\":false,\"hardcore\":false,\"onlineAuth\":true,\"selfSignedAuth\":false,"
+            + "\"nonce\":\"aa394c0f96eb55b8\",\"connection\":4}";
+
+    private static NetherNetServerStatus.Builder vanillaPublic() {
+        return NetherNetServerStatus.builder()
+                .name("Dedicated Server")
+                .protocol(2181)
+                .version("1.26.50")
+                .level("Bedrock level")
+                .players(0)
+                .maxPlayers(10)
+                .gameType(NetherNetServerStatus.GAME_TYPE_SURVIVAL);
+    }
+
+    @Test
+    void publicMembersMatchVanillaByteForByte() {
+        assertEquals(VANILLA_PUBLIC, vanillaPublic().build().toJson());
+    }
+
+    @Test
+    void extendedMembersMatchVanillaInRegistrationOrder() {
+        String json = vanillaPublic()
+                .dataVersion(7)
+                .editor(false)
+                .hardcore(false)
+                .onlineAuth(true)
+                .selfSignedAuth(false)
+                .nonce("aa394c0f96eb55b8")
+                .connection(NetherNetServerStatus.CONNECTION_LAN_WEBRTC_SIGNALING)
+                .build()
+                .toJson();
+        assertEquals(VANILLA_FULL, json);
+    }
+
+    @Test
+    void unsetFlagIsOmittedWhileExplicitFalseIsEmitted() {
+        // Absent and false are different signals, which is why the flags are
+        // boxed rather than plain booleans.
+        assertFalse(vanillaPublic().build().toJson().contains("hardcore"));
+        assertTrue(vanillaPublic().hardcore(false).build().toJson().contains("\"hardcore\":false"));
+        assertTrue(vanillaPublic().hardcore(true).build().toJson().contains("\"hardcore\":true"));
+    }
+
+    @Test
+    void everyMemberIsIndependentlyOptional() {
+        // No member is privileged: setting one never drags in any other, and
+        // an empty status emits an empty document.
+        assertEquals("{}", NetherNetServerStatus.builder().build().toJson());
+        assertEquals("{\"connection\":4}", NetherNetServerStatus.builder()
+                .connection(NetherNetServerStatus.CONNECTION_LAN_WEBRTC_SIGNALING)
+                .build()
+                .toJson());
+        assertEquals("{\"players\":0}", NetherNetServerStatus.builder().players(0).build().toJson());
+    }
+
+    @Test
+    void registrationOrderHoldsForSparseDocuments() {
+        // dataVersion leads the schema even though vanilla filters it out, so
+        // a sparse document still emits in registration order, not set order.
+        assertEquals("{\"dataVersion\":7,\"name\":\"x\",\"nonce\":\"ff\"}", NetherNetServerStatus.builder()
+                .nonce("ff")
+                .name("x")
+                .dataVersion(7)
+                .build()
+                .toJson());
+    }
+
+    @Test
+    void nonceIsUnpaddedLowercaseHex() {
+        for (int i = 0; i < 200; i++) {
+            String nonce = NetherNetServerStatus.randomNonce();
+            assertTrue(nonce.matches("[0-9a-f]{1,16}"), "unexpected nonce: " + nonce);
+            assertFalse(nonce.length() > 1 && nonce.charAt(0) == '0', "zero padded nonce: " + nonce);
+        }
+    }
+}

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/signaling/NetherNetHttpSignalingTest.java
@@ -1,6 +1,7 @@
 package dev.kastle.netty.channel.nethernet.signaling;
 
 import dev.kastle.netty.channel.nethernet.NetherNetServerChannel;
+import dev.kastle.netty.channel.nethernet.NetherNetServerStatus;
 import dev.kastle.netty.channel.nethernet.backend.WebRtcServerBackend;
 import dev.kastle.netty.channel.nethernet.backend.WebRtcSession;
 import dev.kastle.netty.channel.nethernet.backend.WebRtcSessionListener;
@@ -118,6 +119,43 @@ class NetherNetHttpSignalingTest {
     @Test
     void joinProbeAnswers200() throws Exception {
         assertEquals(200, get("/v1/join").statusCode());
+    }
+
+    @Test
+    void joinProbeCarriesTheServerStatusWhenSupplied() throws Exception {
+        try {
+            // No supplier: the historical empty body, which every client
+            // accepts since the guide defines it as ignored.
+            HttpResponse<String> bare = get("/v1/join");
+            assertEquals(200, bare.statusCode());
+            assertEquals("", bare.body());
+
+            signaling.setStatusSupplier(() -> NetherNetServerStatus.builder()
+                    .name("Test Server")
+                    .protocol(1001)
+                    .version("1.26.40")
+                    .level("Test Level")
+                    .players(3)
+                    .maxPlayers(20)
+                    .gameType(NetherNetServerStatus.GAME_TYPE_SURVIVAL)
+                    .build());
+            HttpResponse<String> described = get("/v1/join");
+            assertEquals(200, described.statusCode());
+            assertEquals("application/json", described.headers().firstValue("content-type").orElse(""));
+            assertEquals("{\"name\":\"Test Server\",\"protocol\":1001,\"version\":\"1.26.40\","
+                    + "\"level\":\"Test Level\",\"players\":3,\"maxPlayers\":20,\"gameType\":0}", described.body());
+
+            // A failing supplier must not turn the capability check negative,
+            // or the client stops attempting NetherNet entirely.
+            signaling.setStatusSupplier(() -> {
+                throw new IllegalStateException("no status available");
+            });
+            HttpResponse<String> degraded = get("/v1/join");
+            assertEquals(200, degraded.statusCode());
+            assertEquals("", degraded.body());
+        } finally {
+            signaling.setStatusSupplier(null);
+        }
     }
 
     @Test

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/signaling/NetherNetHttpSignaling.java
@@ -1,6 +1,7 @@
 package org.cloudburstmc.netty.channel.nethernet.signaling;
 
 import org.cloudburstmc.netty.channel.nethernet.NetherNetConstants;
+import org.cloudburstmc.netty.channel.nethernet.NetherNetServerStatus;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
@@ -48,7 +49,10 @@ import java.util.function.Supplier;
  *
  * Endpoints:
  * - GET /v1/join: any 2xx means NetherNet is supported and the client
- *   proceeds with the SDP exchange.
+ *   proceeds with the SDP exchange. Carries the
+ *   {@link NetherNetServerStatus} document when a supplier is set, which
+ *   vanilla 1.26.50 and later answer with as their equivalent of the RakNet
+ *   unconnected pong; older clients ignore the body.
  * - POST /v1/join/{networkId}: the request body is the client's SDP offer;
  *   the response body is the full ICE SDP answer (application/sdp). The
  *   whole exchange fits one round trip, so {@link #fullIceAnswers()} is true
@@ -80,6 +84,9 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
     private final Map<Long, SignalHandler> signalHandlers = new ConcurrentHashMap<>();
     private final Map<Long, PendingExchange> pendingExchanges = new ConcurrentHashMap<>();
 
+    private volatile Supplier<NetherNetServerStatus> statusSupplier;
+    // Racing a duplicate first warn is harmless; this only bounds the spam.
+    private volatile boolean statusSupplierWarned;
     private volatile NewConnectionHandler newConnectionHandler;
     private volatile Channel serverChannel;
     // The TCP accept loop. Owned: bind() is called from the NetherNet server
@@ -110,6 +117,21 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
     public NetherNetHttpSignaling(Supplier<SslContext> sslContextSupplier, EventLoopGroup workerGroup) {
         this.sslContextSupplier = sslContextSupplier;
         this.workerGroup = workerGroup;
+    }
+
+    /**
+     * Sets the source of the server status answered on the capability check,
+     * or null to answer with an empty body as before. Consulted per request,
+     * so live values (the player count above all) need no republishing; the
+     * consumer owns any caching, since this endpoint is reachable by anyone
+     * who can open a TCP connection.
+     *
+     * Called from connection I/O threads; implementations must be thread
+     * safe and return promptly. A failure is treated as no status being
+     * available and never turns the capability check negative.
+     */
+    public void setStatusSupplier(Supplier<NetherNetServerStatus> statusSupplier) {
+        this.statusSupplier = statusSupplier;
     }
 
     @Override
@@ -293,7 +315,7 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
                 if (newConnectionHandler == null || closed) {
                     respond(ctx, HttpResponseStatus.SERVICE_UNAVAILABLE, "text/plain", "Service unavailable");
                 } else {
-                    respond(ctx, HttpResponseStatus.OK, "text/plain", "");
+                    respondStatus(ctx);
                 }
                 return;
             }
@@ -372,6 +394,46 @@ public class NetherNetHttpSignaling implements NetherNetServerSignaling {
      * after the write, matching the one request per connection model. Safe
      * from any thread.
      */
+    /**
+     * Answers the capability check, carrying the server status document when
+     * the consumer supplies one. A supplier that is absent or that fails
+     * leaves the historical empty body, which every client accepts: the guide
+     * defines the body as ignored, and only 1.26.50 and later read it.
+     */
+    private void respondStatus(ChannelHandlerContext ctx) {
+        Supplier<NetherNetServerStatus> supplier = this.statusSupplier;
+        if (supplier == null) {
+            respond(ctx, HttpResponseStatus.OK, "text/plain", "");
+            return;
+        }
+        String body;
+        try {
+            NetherNetServerStatus status = supplier.get();
+            body = status == null ? null : status.toJson();
+        } catch (Exception e) {
+            // The probe decides whether the client attempts NetherNet at all,
+            // so it must still answer 2xx when the consumer cannot describe
+            // itself. Older clients ignore the body outright; what a reading
+            // client makes of an absent one is not established.
+            //
+            // Warn once, then debug: the endpoint is unauthenticated TCP, so
+            // per request warns would let anyone drive log volume by
+            // hammering the probe against a broken supplier.
+            if (statusSupplierWarned) {
+                log.debug("Server status supplier failed: {}", e.getMessage());
+            } else {
+                statusSupplierWarned = true;
+                log.warn("Server status supplier failed (further failures log at debug): {}", e.getMessage());
+            }
+            body = null;
+        }
+        if (body == null) {
+            respond(ctx, HttpResponseStatus.OK, "text/plain", "");
+        } else {
+            respond(ctx, HttpResponseStatus.OK, "application/json", body);
+        }
+    }
+
     private static void respond(ChannelHandlerContext ctx, HttpResponseStatus status, String contentType, String body) {
         FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, status,
                 Unpooled.copiedBuffer(body, StandardCharsets.UTF_8));

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/NetherNetServerStatusTest.java
@@ -1,0 +1,97 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Locks the wire form of the server status document against the shapes
+ * observed from vanilla 1.26.50.
+ */
+class NetherNetServerStatusTest {
+
+    /** Exactly what a vanilla dedicated server answers, key order included. */
+    private static final String VANILLA_PUBLIC = "{\"name\":\"Dedicated Server\",\"protocol\":2181,"
+            + "\"version\":\"1.26.50\",\"level\":\"Bedrock level\",\"players\":0,\"maxPlayers\":10,\"gameType\":0}";
+
+    /** The full document, recovered from vanilla by widening the context mask. */
+    private static final String VANILLA_FULL = "{\"dataVersion\":7,\"name\":\"Dedicated Server\",\"protocol\":2181,"
+            + "\"version\":\"1.26.50\",\"level\":\"Bedrock level\",\"players\":0,\"maxPlayers\":10,\"gameType\":0,"
+            + "\"editor\":false,\"hardcore\":false,\"onlineAuth\":true,\"selfSignedAuth\":false,"
+            + "\"nonce\":\"aa394c0f96eb55b8\",\"connection\":4}";
+
+    private static NetherNetServerStatus.Builder vanillaPublic() {
+        return NetherNetServerStatus.builder()
+                .name("Dedicated Server")
+                .protocol(2181)
+                .version("1.26.50")
+                .level("Bedrock level")
+                .players(0)
+                .maxPlayers(10)
+                .gameType(NetherNetServerStatus.GAME_TYPE_SURVIVAL);
+    }
+
+    @Test
+    void publicMembersMatchVanillaByteForByte() {
+        assertEquals(VANILLA_PUBLIC, vanillaPublic().build().toJson());
+    }
+
+    @Test
+    void extendedMembersMatchVanillaInRegistrationOrder() {
+        String json = vanillaPublic()
+                .dataVersion(7)
+                .editor(false)
+                .hardcore(false)
+                .onlineAuth(true)
+                .selfSignedAuth(false)
+                .nonce("aa394c0f96eb55b8")
+                .connection(NetherNetServerStatus.CONNECTION_LAN_WEBRTC_SIGNALING)
+                .build()
+                .toJson();
+        assertEquals(VANILLA_FULL, json);
+    }
+
+    @Test
+    void unsetFlagIsOmittedWhileExplicitFalseIsEmitted() {
+        // Absent and false are different signals, which is why the flags are
+        // boxed rather than plain booleans.
+        assertFalse(vanillaPublic().build().toJson().contains("hardcore"));
+        assertTrue(vanillaPublic().hardcore(false).build().toJson().contains("\"hardcore\":false"));
+        assertTrue(vanillaPublic().hardcore(true).build().toJson().contains("\"hardcore\":true"));
+    }
+
+    @Test
+    void everyMemberIsIndependentlyOptional() {
+        // No member is privileged: setting one never drags in any other, and
+        // an empty status emits an empty document.
+        assertEquals("{}", NetherNetServerStatus.builder().build().toJson());
+        assertEquals("{\"connection\":4}", NetherNetServerStatus.builder()
+                .connection(NetherNetServerStatus.CONNECTION_LAN_WEBRTC_SIGNALING)
+                .build()
+                .toJson());
+        assertEquals("{\"players\":0}", NetherNetServerStatus.builder().players(0).build().toJson());
+    }
+
+    @Test
+    void registrationOrderHoldsForSparseDocuments() {
+        // dataVersion leads the schema even though vanilla filters it out, so
+        // a sparse document still emits in registration order, not set order.
+        assertEquals("{\"dataVersion\":7,\"name\":\"x\",\"nonce\":\"ff\"}", NetherNetServerStatus.builder()
+                .nonce("ff")
+                .name("x")
+                .dataVersion(7)
+                .build()
+                .toJson());
+    }
+
+    @Test
+    void nonceIsUnpaddedLowercaseHex() {
+        for (int i = 0; i < 200; i++) {
+            String nonce = NetherNetServerStatus.randomNonce();
+            assertTrue(nonce.matches("[0-9a-f]{1,16}"), "unexpected nonce: " + nonce);
+            assertFalse(nonce.length() > 1 && nonce.charAt(0) == '0', "zero padded nonce: " + nonce);
+        }
+    }
+}

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/signaling/NetherNetHttpSignalingTest.java
@@ -1,6 +1,7 @@
 package org.cloudburstmc.netty.channel.nethernet.signaling;
 
 import org.cloudburstmc.netty.channel.nethernet.NetherNetServerChannel;
+import org.cloudburstmc.netty.channel.nethernet.NetherNetServerStatus;
 import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcServerBackend;
 import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSession;
 import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcSessionListener;
@@ -118,6 +119,43 @@ class NetherNetHttpSignalingTest {
     @Test
     void joinProbeAnswers200() throws Exception {
         assertEquals(200, get("/v1/join").statusCode());
+    }
+
+    @Test
+    void joinProbeCarriesTheServerStatusWhenSupplied() throws Exception {
+        try {
+            // No supplier: the historical empty body, which every client
+            // accepts since the guide defines it as ignored.
+            HttpResponse<String> bare = get("/v1/join");
+            assertEquals(200, bare.statusCode());
+            assertEquals("", bare.body());
+
+            signaling.setStatusSupplier(() -> NetherNetServerStatus.builder()
+                    .name("Test Server")
+                    .protocol(1001)
+                    .version("1.26.40")
+                    .level("Test Level")
+                    .players(3)
+                    .maxPlayers(20)
+                    .gameType(NetherNetServerStatus.GAME_TYPE_SURVIVAL)
+                    .build());
+            HttpResponse<String> described = get("/v1/join");
+            assertEquals(200, described.statusCode());
+            assertEquals("application/json", described.headers().firstValue("content-type").orElse(""));
+            assertEquals("{\"name\":\"Test Server\",\"protocol\":1001,\"version\":\"1.26.40\","
+                    + "\"level\":\"Test Level\",\"players\":3,\"maxPlayers\":20,\"gameType\":0}", described.body());
+
+            // A failing supplier must not turn the capability check negative,
+            // or the client stops attempting NetherNet entirely.
+            signaling.setStatusSupplier(() -> {
+                throw new IllegalStateException("no status available");
+            });
+            HttpResponse<String> degraded = get("/v1/join");
+            assertEquals(200, degraded.statusCode());
+            assertEquals("", degraded.body());
+        } finally {
+            signaling.setStatusSupplier(null);
+        }
     }
 
     @Test