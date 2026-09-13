SendableMetatype/NetworkCompatible

Expose the transport round trip time on NetherNet channels
Channels sample the selected ICE candidate pair's currentRoundTripTime
every 3 seconds while active, through a new WebRtcSession.requestRtt
seam (default: no measurement) implemented with peer connection stats.
The value is published as NetherNetChannel#rttMillis for consumers like
Geyser's ping. It is measured by the transport's STUN checks below any
client processing, matching what RakNet's connected ping reports. The
sampler starts before channelActive fires (so a synchronous close from
a handler still cancels it) and on the already active registration
path, and is cancelled on close. Failed samples keep the last good
value; negative means no measurement yet.

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetClientChannel.java
@@ -109,6 +109,21 @@ public class NetherNetClientChannel extends NetherNetChannel {
     }
 
     @Override
+    protected void requestRttSample(java.util.function.DoubleConsumer callback) {
+        RTCPeerConnection pc = this.peerConnection;
+        if (pc == null) {
+            callback.accept(-1);
+            return;
+        }
+        try {
+            pc.getStats(report -> callback.accept(
+                    dev.kastle.netty.channel.nethernet.backend.WebRtcRtt.extractRttMillis(report)));
+        } catch (Exception e) {
+            callback.accept(-1);
+        }
+    }
+
+    @Override
     protected void doClose() throws Exception {
         super.doClose();
         RTCDataChannel reliable = this.reliableChannel;

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/backend/WebRtcRtt.java
@@ -1,0 +1,61 @@
+package dev.kastle.netty.channel.nethernet.backend;
+
+import dev.kastle.webrtc.RTCStats;
+import dev.kastle.webrtc.RTCStatsReport;
+import dev.kastle.webrtc.RTCStatsType;
+
+import java.util.Map;
+
+/**
+ * Extracts the active candidate pair's round trip time from a WebRTC stats
+ * report. libwebrtc refreshes the value with its periodic STUN checks, so it
+ * measures the pure network path below any client processing.
+ */
+public final class WebRtcRtt {
+
+    private WebRtcRtt() {
+    }
+
+    /**
+     * @return the selected candidate pair's current RTT in milliseconds, or
+     *         a negative value when the report carries no measurement
+     */
+    public static double extractRttMillis(RTCStatsReport report) {
+        Map<String, RTCStats> stats = report.getStats();
+
+        RTCStats pair = null;
+        for (RTCStats s : stats.values()) {
+            if (s.getType() == RTCStatsType.TRANSPORT) {
+                Object id = s.getAttributes().get("selectedCandidatePairId");
+                if (id instanceof String selected) {
+                    pair = stats.get(selected);
+                }
+                break;
+            }
+        }
+
+        if (pair == null) {
+            for (RTCStats s : stats.values()) {
+                if (s.getType() != RTCStatsType.CANDIDATE_PAIR) {
+                    continue;
+                }
+                Map<String, Object> attrs = s.getAttributes();
+                Object state = attrs.get("state");
+                boolean succeeded = state == null || "succeeded".equalsIgnoreCase(String.valueOf(state));
+                if (Boolean.TRUE.equals(attrs.get("nominated")) && succeeded) {
+                    pair = s;
+                    break;
+                }
+            }
+        }
+
+        if (pair == null) {
+            return -1;
+        }
+        Object rtt = pair.getAttributes().get("currentRoundTripTime");
+        if (rtt instanceof Number number) {
+            return number.doubleValue() * 1000.0;
+        }
+        return -1;
+    }
+}

transport-nethernet/src/test/java/dev/kastle/webrtc/WebRtcRttTest.java
@@ -1,0 +1,58 @@
+package dev.kastle.webrtc;
+
+import dev.kastle.netty.channel.nethernet.backend.WebRtcRtt;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WebRtcRttTest {
+
+    @Test
+    void usesTransportSelectedCandidatePair() {
+        Map<String, RTCStats> stats = new LinkedHashMap<>();
+        stats.put("fallback", candidatePair("fallback", true, "succeeded", 0.100));
+        stats.put("selected", candidatePair("selected", false, "succeeded", 0.0255));
+        stats.put("transport", stat(RTCStatsType.TRANSPORT, "transport",
+                Map.of("selectedCandidatePairId", "selected")));
+
+        assertEquals(25.5, WebRtcRtt.extractRttMillis(report(stats)), 0.0001);
+    }
+
+    @Test
+    void fallsBackToNominatedSucceededCandidatePair() {
+        Map<String, RTCStats> stats = new LinkedHashMap<>();
+        stats.put("failed", candidatePair("failed", true, "failed", 0.100));
+        stats.put("not-nominated", candidatePair("not-nominated", false, "succeeded", 0.200));
+        stats.put("active", candidatePair("active", true, "SuCcEeDeD", 0.0175));
+
+        assertEquals(17.5, WebRtcRtt.extractRttMillis(report(stats)), 0.0001);
+    }
+
+    @Test
+    void returnsUnavailableWithoutRoundTripTime() {
+        Map<String, RTCStats> stats = Map.of(
+                "transport", stat(RTCStatsType.TRANSPORT, "transport",
+                        Map.of("selectedCandidatePairId", "selected")),
+                "selected", stat(RTCStatsType.CANDIDATE_PAIR, "selected", Map.of()));
+
+        assertEquals(-1, WebRtcRtt.extractRttMillis(report(stats)));
+    }
+
+    private static RTCStats candidatePair(String id, boolean nominated, String state, double rttSeconds) {
+        return stat(RTCStatsType.CANDIDATE_PAIR, id, Map.of(
+                "nominated", nominated,
+                "state", state,
+                "currentRoundTripTime", rttSeconds));
+    }
+
+    private static RTCStats stat(RTCStatsType type, String id, Map<String, Object> attributes) {
+        return new RTCStats(0, type, id, attributes);
+    }
+
+    private static RTCStatsReport report(Map<String, RTCStats> stats) {
+        return new RTCStatsReport(stats, 0);
+    }
+}

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/NetherNetChannelRttSamplerTest.java
@@ -1,0 +1,115 @@
+package dev.kastle.netty.channel.nethernet;
+
+import dev.kastle.netty.channel.nethernet.config.DefaultNetherChannelConfig;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.DoubleConsumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NetherNetChannelRttSamplerTest {
+
+    private final NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+    @AfterEach
+    void tearDown() {
+        group.shutdownGracefully(0, 1, TimeUnit.SECONDS).syncUninterruptibly();
+    }
+
+    @Test
+    void startsSamplerWhenTransportOpensAfterRegistration() throws Exception {
+        TestNetherNetChannel channel = new TestNetherNetChannel();
+        try {
+            group.register(channel).sync();
+            channel.openTransport();
+
+            assertTrue(channel.sampled.await(2, TimeUnit.SECONDS));
+            assertEquals(13, channel.rttMillis());
+            assertEquals(1, channel.sampleRequests.get());
+        } finally {
+            channel.close().syncUninterruptibly();
+        }
+    }
+
+    @Test
+    void startsSamplerWhenTransportOpensBeforeRegistration() throws Exception {
+        TestNetherNetChannel channel = new TestNetherNetChannel();
+        try {
+            channel.openTransport();
+            group.register(channel).sync();
+
+            assertTrue(channel.sampled.await(2, TimeUnit.SECONDS));
+            assertEquals(13, channel.rttMillis());
+            assertEquals(1, channel.sampleRequests.get());
+        } finally {
+            channel.close().syncUninterruptibly();
+        }
+    }
+
+    @Test
+    void synchronousCloseFromChannelActiveCancelsSampler() throws Exception {
+        TestNetherNetChannel channel = new TestNetherNetChannel();
+        channel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) {
+                ctx.close();
+            }
+        });
+
+        group.register(channel).sync();
+        channel.openTransport();
+        channel.closeFuture().sync();
+
+        assertFalse(channel.sampled.await(1200, TimeUnit.MILLISECONDS));
+        assertEquals(0, channel.sampleRequests.get());
+    }
+
+    private static final class TestNetherNetChannel extends NetherNetChannel {
+        private final CountDownLatch sampled = new CountDownLatch(1);
+        private final AtomicInteger sampleRequests = new AtomicInteger();
+
+        private TestNetherNetChannel() {
+            super(null, new InetSocketAddress("127.0.0.1", 19132),
+                    new InetSocketAddress("127.0.0.1", 19133));
+            this.config = new DefaultNetherChannelConfig(this);
+        }
+
+        private void openTransport() {
+            markTransportOpen();
+        }
+
+        @Override
+        protected void requestRttSample(DoubleConsumer callback) {
+            sampleRequests.incrementAndGet();
+            callback.accept(12.6);
+            sampled.countDown();
+        }
+
+        @Override
+        protected void sendFramed(ByteBuf framed) {
+        }
+
+        @Override
+        protected AbstractUnsafe newUnsafe() {
+            return new AbstractUnsafe() {
+                @Override
+                public void connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
+                    promise.setFailure(new UnsupportedOperationException("Test channel cannot connect"));
+                }
+            };
+        }
+    }
+}

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetClientChannel.java
@@ -109,6 +109,21 @@ public class NetherNetClientChannel extends NetherNetChannel {
     }
 
     @Override
+    protected void requestRttSample(java.util.function.DoubleConsumer callback) {
+        RTCPeerConnection pc = this.peerConnection;
+        if (pc == null) {
+            callback.accept(-1);
+            return;
+        }
+        try {
+            pc.getStats(report -> callback.accept(
+                    org.cloudburstmc.netty.channel.nethernet.backend.WebRtcRtt.extractRttMillis(report)));
+        } catch (Exception e) {
+            callback.accept(-1);
+        }
+    }
+
+    @Override
     protected void doClose() throws Exception {
         super.doClose();
         RTCDataChannel reliable = this.reliableChannel;

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/backend/WebRtcRtt.java
@@ -1,0 +1,61 @@
+package org.cloudburstmc.netty.channel.nethernet.backend;
+
+import dev.kastle.webrtc.RTCStats;
+import dev.kastle.webrtc.RTCStatsReport;
+import dev.kastle.webrtc.RTCStatsType;
+
+import java.util.Map;
+
+/**
+ * Extracts the active candidate pair's round trip time from a WebRTC stats
+ * report. libwebrtc refreshes the value with its periodic STUN checks, so it
+ * measures the pure network path below any client processing.
+ */
+public final class WebRtcRtt {
+
+    private WebRtcRtt() {
+    }
+
+    /**
+     * @return the selected candidate pair's current RTT in milliseconds, or
+     *         a negative value when the report carries no measurement
+     */
+    public static double extractRttMillis(RTCStatsReport report) {
+        Map<String, RTCStats> stats = report.getStats();
+
+        RTCStats pair = null;
+        for (RTCStats s : stats.values()) {
+            if (s.getType() == RTCStatsType.TRANSPORT) {
+                Object id = s.getAttributes().get("selectedCandidatePairId");
+                if (id instanceof String selected) {
+                    pair = stats.get(selected);
+                }
+                break;
+            }
+        }
+
+        if (pair == null) {
+            for (RTCStats s : stats.values()) {
+                if (s.getType() != RTCStatsType.CANDIDATE_PAIR) {
+                    continue;
+                }
+                Map<String, Object> attrs = s.getAttributes();
+                Object state = attrs.get("state");
+                boolean succeeded = state == null || "succeeded".equalsIgnoreCase(String.valueOf(state));
+                if (Boolean.TRUE.equals(attrs.get("nominated")) && succeeded) {
+                    pair = s;
+                    break;
+                }
+            }
+        }
+
+        if (pair == null) {
+            return -1;
+        }
+        Object rtt = pair.getAttributes().get("currentRoundTripTime");
+        if (rtt instanceof Number number) {
+            return number.doubleValue() * 1000.0;
+        }
+        return -1;
+    }
+}

transport-nethernet/src/test/java/dev/kastle/webrtc/WebRtcRttTest.java
@@ -1,0 +1,58 @@
+package dev.kastle.webrtc;
+
+import org.cloudburstmc.netty.channel.nethernet.backend.WebRtcRtt;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WebRtcRttTest {
+
+    @Test
+    void usesTransportSelectedCandidatePair() {
+        Map<String, RTCStats> stats = new LinkedHashMap<>();
+        stats.put("fallback", candidatePair("fallback", true, "succeeded", 0.100));
+        stats.put("selected", candidatePair("selected", false, "succeeded", 0.0255));
+        stats.put("transport", stat(RTCStatsType.TRANSPORT, "transport",
+                Map.of("selectedCandidatePairId", "selected")));
+
+        assertEquals(25.5, WebRtcRtt.extractRttMillis(report(stats)), 0.0001);
+    }
+
+    @Test
+    void fallsBackToNominatedSucceededCandidatePair() {
+        Map<String, RTCStats> stats = new LinkedHashMap<>();
+        stats.put("failed", candidatePair("failed", true, "failed", 0.100));
+        stats.put("not-nominated", candidatePair("not-nominated", false, "succeeded", 0.200));
+        stats.put("active", candidatePair("active", true, "SuCcEeDeD", 0.0175));
+
+        assertEquals(17.5, WebRtcRtt.extractRttMillis(report(stats)), 0.0001);
+    }
+
+    @Test
+    void returnsUnavailableWithoutRoundTripTime() {
+        Map<String, RTCStats> stats = Map.of(
+                "transport", stat(RTCStatsType.TRANSPORT, "transport",
+                        Map.of("selectedCandidatePairId", "selected")),
+                "selected", stat(RTCStatsType.CANDIDATE_PAIR, "selected", Map.of()));
+
+        assertEquals(-1, WebRtcRtt.extractRttMillis(report(stats)));
+    }
+
+    private static RTCStats candidatePair(String id, boolean nominated, String state, double rttSeconds) {
+        return stat(RTCStatsType.CANDIDATE_PAIR, id, Map.of(
+                "nominated", nominated,
+                "state", state,
+                "currentRoundTripTime", rttSeconds));
+    }
+
+    private static RTCStats stat(RTCStatsType type, String id, Map<String, Object> attributes) {
+        return new RTCStats(0, type, id, attributes);
+    }
+
+    private static RTCStatsReport report(Map<String, RTCStats> stats) {
+        return new RTCStatsReport(stats, 0);
+    }
+}

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChannelRttSamplerTest.java
@@ -1,0 +1,115 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+import org.cloudburstmc.netty.channel.nethernet.config.DefaultNetherChannelConfig;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.DoubleConsumer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NetherNetChannelRttSamplerTest {
+
+    private final NioEventLoopGroup group = new NioEventLoopGroup(1);
+
+    @AfterEach
+    void tearDown() {
+        group.shutdownGracefully(0, 1, TimeUnit.SECONDS).syncUninterruptibly();
+    }
+
+    @Test
+    void startsSamplerWhenTransportOpensAfterRegistration() throws Exception {
+        TestNetherNetChannel channel = new TestNetherNetChannel();
+        try {
+            group.register(channel).sync();
+            channel.openTransport();
+
+            assertTrue(channel.sampled.await(2, TimeUnit.SECONDS));
+            assertEquals(13, channel.rttMillis());
+            assertEquals(1, channel.sampleRequests.get());
+        } finally {
+            channel.close().syncUninterruptibly();
+        }
+    }
+
+    @Test
+    void startsSamplerWhenTransportOpensBeforeRegistration() throws Exception {
+        TestNetherNetChannel channel = new TestNetherNetChannel();
+        try {
+            channel.openTransport();
+            group.register(channel).sync();
+
+            assertTrue(channel.sampled.await(2, TimeUnit.SECONDS));
+            assertEquals(13, channel.rttMillis());
+            assertEquals(1, channel.sampleRequests.get());
+        } finally {
+            channel.close().syncUninterruptibly();
+        }
+    }
+
+    @Test
+    void synchronousCloseFromChannelActiveCancelsSampler() throws Exception {
+        TestNetherNetChannel channel = new TestNetherNetChannel();
+        channel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) {
+                ctx.close();
+            }
+        });
+
+        group.register(channel).sync();
+        channel.openTransport();
+        channel.closeFuture().sync();
+
+        assertFalse(channel.sampled.await(1200, TimeUnit.MILLISECONDS));
+        assertEquals(0, channel.sampleRequests.get());
+    }
+
+    private static final class TestNetherNetChannel extends NetherNetChannel {
+        private final CountDownLatch sampled = new CountDownLatch(1);
+        private final AtomicInteger sampleRequests = new AtomicInteger();
+
+        private TestNetherNetChannel() {
+            super(null, new InetSocketAddress("127.0.0.1", 19132),
+                    new InetSocketAddress("127.0.0.1", 19133));
+            this.config = new DefaultNetherChannelConfig(this);
+        }
+
+        private void openTransport() {
+            markTransportOpen();
+        }
+
+        @Override
+        protected void requestRttSample(DoubleConsumer callback) {
+            sampleRequests.incrementAndGet();
+            callback.accept(12.6);
+            sampled.countDown();
+        }
+
+        @Override
+        protected void sendFramed(ByteBuf framed) {
+        }
+
+        @Override
+        protected AbstractUnsafe newUnsafe() {
+            return new AbstractUnsafe() {
+                @Override
+                public void connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
+                    promise.setFailure(new UnsupportedOperationException("Test channel cannot connect"));
+                }
+            };
+        }
+    }
+}