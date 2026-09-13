Kas-tle/NetworkCompatible

Add Nethernet support (netty-transport-nethernet) (#4)
* Add transport-nethernet package

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Correct discovery packet length and properly encode discovery string length as varint

Co-authored-by: RaphiMC <50594595+RaphiMC@users.noreply.github.com>
Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Refactor NetherNetClientChannel using RTCDataChannelObserver to succeed connectPromise

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Encourage use of single PeerConnectionFactory

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Switch to data only webrtc lib

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Refactor to support joining realms

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Fix issues with connectionId being signed in SDP strings; fix LAN server channel

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Expose ice servers in NetherNetXboxSignaling

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Remove native depends

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* More robust logging and ICE/TURN parsing

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Properly track NetherNetServerChannel state

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Prep for nethernet release

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Fix option access

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Add max retries for client connect

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Fail connect promise if target network id is not found

Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

* Add case for Delivery Acknowledgeme

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetChannelFactory.java
@@ -1,0 +1,45 @@
+package dev.kastle.netty.channel.nethernet;
+
+import dev.kastle.netty.channel.nethernet.signaling.NetherNetClientSignaling;
+import dev.kastle.netty.channel.nethernet.signaling.NetherNetServerSignaling;
+import dev.kastle.webrtc.PeerConnectionFactory;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFactory;
+
+import java.util.function.Supplier;
+
+public class NetherNetChannelFactory<T extends Channel> implements ChannelFactory<T> {
+
+    private final Supplier<T> channelCreator;
+
+    private NetherNetChannelFactory(Supplier<T> channelCreator) {
+        this.channelCreator = channelCreator;
+    }
+
+    @Override
+    public T newChannel() {
+        return channelCreator.get();
+    }
+
+    /**
+     * Creates a NetherNet Server Channel Factory.
+     * 
+     * @param factory The PeerConnectionFactory to use for creating peer connections. Should be reused where possible.
+     * @param signaling The NetherNetServerSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetServerChannel.
+     */
+    public static ChannelFactory<NetherNetServerChannel> server(PeerConnectionFactory factory, NetherNetServerSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(factory, signaling));
+    }
+
+    /**
+     * Creates a NetherNet Client Channel Factory.
+     * 
+     * @param factory The PeerConnectionFactory to use for creating peer connections. Should be reused where possible.
+     * @param signaling The NetherNetClientSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetClientChannel.
+     */
+    public static ChannelFactory<NetherNetClientChannel> client(PeerConnectionFactory factory, NetherNetClientSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetClientChannel(factory, signaling));
+    }
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetChildChannel.java
@@ -1,0 +1,32 @@
+package dev.kastle.netty.channel.nethernet;
+
+import dev.kastle.netty.channel.nethernet.config.DefaultNetherChannelConfig;
+import dev.kastle.webrtc.RTCPeerConnection;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelPromise;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+public class NetherNetChildChannel extends NetherNetChannel {
+    public NetherNetChildChannel(Channel parent, RTCPeerConnection peerConnection, InetSocketAddress remote, InetSocketAddress local) {
+        super(parent, remote, local);
+        this.peerConnection = peerConnection;
+        this.config = new DefaultNetherChannelConfig(this);
+    }
+
+    @Override
+    protected AbstractUnsafe newUnsafe() {
+        return new AbstractUnsafe() {
+            @Override
+            public void connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
+                promise.setFailure(new UnsupportedOperationException("Child channel cannot connect"));
+            }
+        };
+    }
+
+    @Override
+    protected void doBind(SocketAddress localAddress) throws Exception {
+        throw new UnsupportedOperationException("Child channel cannot be bound");
+    }
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/config/DefaultNetherChannelConfig.java
@@ -1,0 +1,34 @@
+package dev.kastle.netty.channel.nethernet.config;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.DefaultChannelConfig;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DefaultNetherChannelConfig extends DefaultChannelConfig {
+    private final Map<ChannelOption<?>, Object> options = new ConcurrentHashMap<>();
+
+    public DefaultNetherChannelConfig(Channel channel) {
+        super(channel);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getOption(ChannelOption<T> option) {
+        if (options.containsKey(option)) {
+            return (T) options.get(option);
+        }
+        return super.getOption(option);
+    }
+
+    @Override
+    public <T> boolean setOption(ChannelOption<T> option, T value) {
+        if (super.setOption(option, value)) {
+            return true;
+        }
+        options.put(option, value);
+        return true;
+    }
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/signaling/NetherNetServerSignaling.java
@@ -1,0 +1,130 @@
+package dev.kastle.netty.channel.nethernet.signaling;
+
+import java.net.ConnectException;
+import java.net.SocketAddress;
+import java.util.List;
+
+public interface NetherNetServerSignaling extends NetherNetSignaling {
+    /**
+     * Binds the signaling medium to listen for incoming connections (Server mode).
+     * 
+     * @param localAddress The local address to bind to.
+     * @throws ConnectException 
+     */
+    void bind(SocketAddress localAddress) throws ConnectException;
+
+    /**
+     * Handler for new connections.
+     * 
+     * @param handler Functional interface receiving (ConnectionID, RemoteNetworkID, Payload)
+     */
+    void setNewConnectionHandler(NewConnectionHandler handler);
+
+    /**
+     * Sets the advertisement data for the discovery mechanism (e.g. LAN Pong).
+     * 
+     * @param pongData The Pong advertisement data.
+     */
+    void setAdvertisementData(PongData pongData);
+
+    /**
+     * Functional interface for new connection handling.
+     */
+    @FunctionalInterface
+    interface NewConnectionHandler {
+        /**
+         * Called when a new connection is initiated by a remote peer.
+         *
+         * @param connectionId     The unique connection ID for this session.
+         * @param remoteNetworkId  The Network ID of the remote peer.
+         * @param payload          The initial signaling payload from the remote peer.
+         */
+        void onConnect(long connectionId, String remoteNetworkId, String payload);
+    }
+
+    /**
+     * Returns the ICE servers (STUN/TURN) obtained from the signaling handshake.
+     * Returns empty list if none available or not applicable.
+     */
+    default List<IceServerInfo> getIceServers() {
+        return java.util.Collections.emptyList();
+    }
+
+    /**
+     * Data structure for Pong advertisement data.
+     * 
+     * @param serverName      The name of the server.
+     * @param levelName       The name of the level/world.
+     * @param gameType        The game type (e.g. Survival, Creative).
+     * @param playerCount     The current number of players.
+     * @param maxPlayerCount  The maximum number of players allowed.
+     * @param isEditorWorld   Whether the world is an editor world.
+     * @param isHardcore      Whether the world is in hardcore mode.
+     * @param transportLayer  The transport layer identifier (e.g. NetherNet).
+     * @param connectionType  The connection type identifier (e.g. LAN, Online).
+     */
+    public record PongData(String serverName, String levelName, int gameType, int playerCount, int maxPlayerCount, 
+            boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType) {
+        public static class Builder {
+            private String serverName = "Server";
+            private String levelName = "World";
+            private int gameType = 0; // Default to Survival
+            private int playerCount = 0;
+            private int maxPlayerCount = 10;
+            private boolean isEditorWorld = false;
+            private boolean isHardcore = false;
+            private int transportLayer = 2; // Default to NetherNet
+            private int connectionType = 4; // Default to LAN
+
+            public Builder setServerName(String serverName) {
+                this.serverName = serverName;
+                return this;
+            }
+
+            public Builder setLevelName(String levelName) {
+                this.levelName = levelName;
+                return this;
+            }
+
+            public Builder setGameType(int gameType) {
+                this.gameType = gameType;
+                return this;
+            }
+
+            public Builder setPlayerCount(int playerCount) {
+                this.playerCount = playerCount;
+                return this;
+            }
+
+            public Builder setMaxPlayerCount(int maxPlayerCount) {
+                this.maxPlayerCount = maxPlayerCount;
+                return this;
+            }
+
+            public Builder setIsEditorWorld(boolean isEditorWorld) {
+                this.isEditorWorld = isEditorWorld;
+                return this;
+            }
+
+            public Builder setIsHardcore(boolean isHardcore) {
+                this.isHardcore = isHardcore;
+                return this;
+            }
+
+            public Builder setTransportLayer(int transportLayer) {
+                this.transportLayer = transportLayer;
+                return this;
+            }
+
+            public Builder setConnectionType(int connectionType) {
+                this.connectionType = connectionType;
+                return this;
+            }
+
+            public PongData build() {
+                return new PongData(serverName, levelName, gameType, playerCount, maxPlayerCount, 
+                    isEditorWorld, isHardcore, transportLayer, connectionType);
+            }
+        }
+    }
+}

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChannelFactory.java
@@ -1,0 +1,45 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetClientSignaling;
+import org.cloudburstmc.netty.channel.nethernet.signaling.NetherNetServerSignaling;
+import dev.kastle.webrtc.PeerConnectionFactory;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFactory;
+
+import java.util.function.Supplier;
+
+public class NetherNetChannelFactory<T extends Channel> implements ChannelFactory<T> {
+
+    private final Supplier<T> channelCreator;
+
+    private NetherNetChannelFactory(Supplier<T> channelCreator) {
+        this.channelCreator = channelCreator;
+    }
+
+    @Override
+    public T newChannel() {
+        return channelCreator.get();
+    }
+
+    /**
+     * Creates a NetherNet Server Channel Factory.
+     * 
+     * @param factory The PeerConnectionFactory to use for creating peer connections. Should be reused where possible.
+     * @param signaling The NetherNetServerSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetServerChannel.
+     */
+    public static ChannelFactory<NetherNetServerChannel> server(PeerConnectionFactory factory, NetherNetServerSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetServerChannel(factory, signaling));
+    }
+
+    /**
+     * Creates a NetherNet Client Channel Factory.
+     * 
+     * @param factory The PeerConnectionFactory to use for creating peer connections. Should be reused where possible.
+     * @param signaling The NetherNetClientSignaling instance for signaling.
+     * @return A ChannelFactory for NetherNetClientChannel.
+     */
+    public static ChannelFactory<NetherNetClientChannel> client(PeerConnectionFactory factory, NetherNetClientSignaling signaling) {
+        return new NetherNetChannelFactory<>(() -> new NetherNetClientChannel(factory, signaling));
+    }
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetChildChannel.java
@@ -1,0 +1,32 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+import org.cloudburstmc.netty.channel.nethernet.config.DefaultNetherChannelConfig;
+import dev.kastle.webrtc.RTCPeerConnection;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelPromise;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
+public class NetherNetChildChannel extends NetherNetChannel {
+    public NetherNetChildChannel(Channel parent, RTCPeerConnection peerConnection, InetSocketAddress remote, InetSocketAddress local) {
+        super(parent, remote, local);
+        this.peerConnection = peerConnection;
+        this.config = new DefaultNetherChannelConfig(this);
+    }
+
+    @Override
+    protected AbstractUnsafe newUnsafe() {
+        return new AbstractUnsafe() {
+            @Override
+            public void connect(SocketAddress remoteAddress, SocketAddress localAddress, ChannelPromise promise) {
+                promise.setFailure(new UnsupportedOperationException("Child channel cannot connect"));
+            }
+        };
+    }
+
+    @Override
+    protected void doBind(SocketAddress localAddress) throws Exception {
+        throw new UnsupportedOperationException("Child channel cannot be bound");
+    }
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/config/DefaultNetherChannelConfig.java
@@ -1,0 +1,34 @@
+package org.cloudburstmc.netty.channel.nethernet.config;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.DefaultChannelConfig;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DefaultNetherChannelConfig extends DefaultChannelConfig {
+    private final Map<ChannelOption<?>, Object> options = new ConcurrentHashMap<>();
+
+    public DefaultNetherChannelConfig(Channel channel) {
+        super(channel);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getOption(ChannelOption<T> option) {
+        if (options.containsKey(option)) {
+            return (T) options.get(option);
+        }
+        return super.getOption(option);
+    }
+
+    @Override
+    public <T> boolean setOption(ChannelOption<T> option, T value) {
+        if (super.setOption(option, value)) {
+            return true;
+        }
+        options.put(option, value);
+        return true;
+    }
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/signaling/NetherNetServerSignaling.java
@@ -1,0 +1,130 @@
+package org.cloudburstmc.netty.channel.nethernet.signaling;
+
+import java.net.ConnectException;
+import java.net.SocketAddress;
+import java.util.List;
+
+public interface NetherNetServerSignaling extends NetherNetSignaling {
+    /**
+     * Binds the signaling medium to listen for incoming connections (Server mode).
+     * 
+     * @param localAddress The local address to bind to.
+     * @throws ConnectException 
+     */
+    void bind(SocketAddress localAddress) throws ConnectException;
+
+    /**
+     * Handler for new connections.
+     * 
+     * @param handler Functional interface receiving (ConnectionID, RemoteNetworkID, Payload)
+     */
+    void setNewConnectionHandler(NewConnectionHandler handler);
+
+    /**
+     * Sets the advertisement data for the discovery mechanism (e.g. LAN Pong).
+     * 
+     * @param pongData The Pong advertisement data.
+     */
+    void setAdvertisementData(PongData pongData);
+
+    /**
+     * Functional interface for new connection handling.
+     */
+    @FunctionalInterface
+    interface NewConnectionHandler {
+        /**
+         * Called when a new connection is initiated by a remote peer.
+         *
+         * @param connectionId     The unique connection ID for this session.
+         * @param remoteNetworkId  The Network ID of the remote peer.
+         * @param payload          The initial signaling payload from the remote peer.
+         */
+        void onConnect(long connectionId, String remoteNetworkId, String payload);
+    }
+
+    /**
+     * Returns the ICE servers (STUN/TURN) obtained from the signaling handshake.
+     * Returns empty list if none available or not applicable.
+     */
+    default List<IceServerInfo> getIceServers() {
+        return java.util.Collections.emptyList();
+    }
+
+    /**
+     * Data structure for Pong advertisement data.
+     * 
+     * @param serverName      The name of the server.
+     * @param levelName       The name of the level/world.
+     * @param gameType        The game type (e.g. Survival, Creative).
+     * @param playerCount     The current number of players.
+     * @param maxPlayerCount  The maximum number of players allowed.
+     * @param isEditorWorld   Whether the world is an editor world.
+     * @param isHardcore      Whether the world is in hardcore mode.
+     * @param transportLayer  The transport layer identifier (e.g. NetherNet).
+     * @param connectionType  The connection type identifier (e.g. LAN, Online).
+     */
+    public record PongData(String serverName, String levelName, int gameType, int playerCount, int maxPlayerCount, 
+            boolean isEditorWorld, boolean isHardcore, int transportLayer, int connectionType) {
+        public static class Builder {
+            private String serverName = "Server";
+            private String levelName = "World";
+            private int gameType = 0; // Default to Survival
+            private int playerCount = 0;
+            private int maxPlayerCount = 10;
+            private boolean isEditorWorld = false;
+            private boolean isHardcore = false;
+            private int transportLayer = 2; // Default to NetherNet
+            private int connectionType = 4; // Default to LAN
+
+            public Builder setServerName(String serverName) {
+                this.serverName = serverName;
+                return this;
+            }
+
+            public Builder setLevelName(String levelName) {
+                this.levelName = levelName;
+                return this;
+            }
+
+            public Builder setGameType(int gameType) {
+                this.gameType = gameType;
+                return this;
+            }
+
+            public Builder setPlayerCount(int playerCount) {
+                this.playerCount = playerCount;
+                return this;
+            }
+
+            public Builder setMaxPlayerCount(int maxPlayerCount) {
+                this.maxPlayerCount = maxPlayerCount;
+                return this;
+            }
+
+            public Builder setIsEditorWorld(boolean isEditorWorld) {
+                this.isEditorWorld = isEditorWorld;
+                return this;
+            }
+
+            public Builder setIsHardcore(boolean isHardcore) {
+                this.isHardcore = isHardcore;
+                return this;
+            }
+
+            public Builder setTransportLayer(int transportLayer) {
+                this.transportLayer = transportLayer;
+                return this;
+            }
+
+            public Builder setConnectionType(int connectionType) {
+                this.connectionType = connectionType;
+                return this;
+            }
+
+            public PongData build() {
+                return new PongData(serverName, levelName, gameType, playerCount, maxPlayerCount, 
+                    isEditorWorld, isHardcore, transportLayer, connectionType);
+            }
+        }
+    }
+}