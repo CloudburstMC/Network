SendableMetatype/NetworkCompatible

Strip identity attributes from offers before the engine
The NetherNet spec instructs removing a=identity before SDP reaches
WebRTC: the assertion is signaling layer metadata, and current client
offers always carry one. The engine tolerated it in live testing, but
stripping removes the dependence on that tolerance. The unstripped
offer stays available above the backend for future validation.

Also fixes the test only constructor ambiguity introduced with the
TLS supplier overload, which broke compileTestJava without failing
JitPack builds.

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/backend/LibWebRtcServerBackendTest.java
@@ -1,0 +1,26 @@
+package dev.kastle.netty.channel.nethernet.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LibWebRtcServerBackendTest {
+
+    @Test
+    void stripsIdentityAttributesOnly() {
+        String offer = "v=0\r\n"
+                + "o=- 1 2 IN IP4 127.0.0.1\r\n"
+                + "a=fingerprint:sha-256 AA:BB\r\n"
+                + "a=identity:eyJpZHAiOnsiZG9tYWluIjoiYXV0aC5taW5lY3JhZnQub3JnIn19\r\n"
+                + "m=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\n"
+                + "a=max-message-size:262144\r\n";
+        String stripped = LibWebRtcServerBackend.stripIdentityAttributes(offer);
+        assertEquals("v=0\r\n"
+                + "o=- 1 2 IN IP4 127.0.0.1\r\n"
+                + "a=fingerprint:sha-256 AA:BB\r\n"
+                + "m=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\n"
+                + "a=max-message-size:262144\r\n", stripped);
+        // No identity present: unchanged.
+        assertEquals(stripped, LibWebRtcServerBackend.stripIdentityAttributes(stripped));
+    }
+}

CloudburstMC/Network

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/backend/LibWebRtcServerBackendTest.java
@@ -1,0 +1,26 @@
+package org.cloudburstmc.netty.channel.nethernet.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LibWebRtcServerBackendTest {
+
+    @Test
+    void stripsIdentityAttributesOnly() {
+        String offer = "v=0\r\n"
+                + "o=- 1 2 IN IP4 127.0.0.1\r\n"
+                + "a=fingerprint:sha-256 AA:BB\r\n"
+                + "a=identity:eyJpZHAiOnsiZG9tYWluIjoiYXV0aC5taW5lY3JhZnQub3JnIn19\r\n"
+                + "m=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\n"
+                + "a=max-message-size:262144\r\n";
+        String stripped = LibWebRtcServerBackend.stripIdentityAttributes(offer);
+        assertEquals("v=0\r\n"
+                + "o=- 1 2 IN IP4 127.0.0.1\r\n"
+                + "a=fingerprint:sha-256 AA:BB\r\n"
+                + "m=application 9 UDP/DTLS/SCTP webrtc-datachannel\r\n"
+                + "a=max-message-size:262144\r\n", stripped);
+        // No identity present: unchanged.
+        assertEquals(stripped, LibWebRtcServerBackend.stripIdentityAttributes(stripped));
+    }
+}