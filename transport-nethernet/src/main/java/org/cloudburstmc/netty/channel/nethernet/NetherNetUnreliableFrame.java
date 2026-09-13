EduGeyser/NetworkM

Receive unreliable NetherNet messages without changing outbound delivery

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/NetherNetUnreliableFrame.java
@@ -1,0 +1,23 @@
+package dev.kastle.netty.channel.nethernet;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.DefaultByteBufHolder;
+
+/**
+ * An inbound unreliable message, including its NetherNet header. The framing
+ * codec decodes it independently of any reliable message being reassembled.
+ * Outbound messages still use reliable delivery and must be plain byte buffers.
+ */
+public final class NetherNetUnreliableFrame extends DefaultByteBufHolder {
+    /**
+     * @param data the raw framed message; this holder takes ownership of its reference
+     */
+    public NetherNetUnreliableFrame(ByteBuf data) {
+        super(data);
+    }
+
+    @Override
+    public NetherNetUnreliableFrame replace(ByteBuf content) {
+        return new NetherNetUnreliableFrame(content);
+    }
+}

transport-nethernet/src/main/java/dev/kastle/netty/channel/nethernet/codec/NetherNetFramingCodec.java
@@ -2,6 +2,7 @@ package dev.kastle.netty.channel.nethernet.codec;
 
 import dev.kastle.netty.channel.nethernet.NetherNetChannel;
 import dev.kastle.netty.channel.nethernet.NetherNetConstants;
+import dev.kastle.netty.channel.nethernet.NetherNetUnreliableFrame;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.channel.ChannelDuplexHandler;
@@ -19,7 +20,9 @@ import io.netty.util.internal.logging.InternalLoggerFactory;
  * fragments still to come after this one. A complete single message is
  * header 0 plus payload; a fragmented message counts down to a final
  * fragment with header 0, at which point the accumulated payload is one
- * complete Bedrock batch.
+ * complete Bedrock batch. Unreliable inbound frames must have header 0 and
+ * bypass reliable reassembly. Decoded messages from either channel are byte
+ * buffers; unreliable messages can arrive while a reliable message is incomplete.
  *
  * Inbound reassembly accumulates retained slices in a composite buffer, so
  * reassembly itself copies nothing. Outbound fragments at the maximum
@@ -87,12 +90,13 @@ public class NetherNetFramingCodec extends ChannelDuplexHandler {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (!(msg instanceof ByteBuf)) {
+        boolean unreliable = msg instanceof NetherNetUnreliableFrame;
+        if (!unreliable && !(msg instanceof ByteBuf)) {
             deliveredMessage = true;
             ctx.fireChannelRead(msg);
             return;
         }
-        ByteBuf buf = (ByteBuf) msg;
+        ByteBuf buf = unreliable ? ((NetherNetUnreliableFrame) msg).content() : (ByteBuf) msg;
         receivedFrame = true;
         try {
             if (!buf.isReadable()) {
@@ -100,6 +104,14 @@ public class NetherNetFramingCodec extends ChannelDuplexHandler {
             }
 
             int header = buf.readUnsignedByte();
+            if (unreliable) {
+                // An unreliable message must never complete or reset a reliable assembly.
+                if (header == 0 && buf.isReadable() && buf.readableBytes() <= MAX_REASSEMBLED_SIZE) {
+                    deliveredMessage = true;
+                    ctx.fireChannelRead(buf.readRetainedSlice(buf.readableBytes()));
+                }
+                return;
+            }
             if (buf.readableBytes() > MAX_REASSEMBLED_SIZE) {
                 log.warn("Inbound message exceeds {} bytes, dropping", MAX_REASSEMBLED_SIZE);
                 resetAssembly();

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/NetherNetClientChannelLifecycleTest.java
@@ -1,5 +1,6 @@
 package dev.kastle.netty.channel.nethernet;
 
+import dev.kastle.netty.channel.nethernet.codec.NetherNetFramingCodec;
 import dev.kastle.netty.channel.nethernet.config.DefaultNetherClientChannelConfig;
 import dev.kastle.netty.channel.nethernet.config.NetherChannelOption;
 import dev.kastle.netty.channel.nethernet.config.NetherNetAddress;
@@ -292,6 +293,63 @@ class NetherNetClientChannelLifecycleTest {
 
     private static RTCDataChannelBuffer message(int value) {
         return new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{(byte) value}), true);
+    }
+
+    @Test
+    void unreliableMessagesWaitForActivationAndApplicationReads() throws Exception {
+        try (Harness h = new Harness()) {
+            h.channel.pipeline().addFirst(new NetherNetFramingCodec());
+            h.channel.config().setAutoRead(false);
+            ChannelFuture connect = h.connect();
+            RTCDataChannelObserver unreliable = h.channel.createUnreliableObserver(
+                    (Integer) field(h.channel, "attemptGeneration"));
+            unreliable.onStateChange();
+            ByteBuffer callback = ByteBuffer.wrap(new byte[]{99, 0, 42, 99});
+            callback.position(1).limit(3);
+            unreliable.onMessage(new RTCDataChannelBuffer(callback, true));
+            callback.put(2, (byte) 43);
+            h.pump();
+            assertFalse(connect.isDone());
+            assertTrue(h.events.isEmpty());
+
+            h.observer(new AtomicReference<>(RTCDataChannelState.OPEN)).onStateChange();
+            h.pump();
+            assertTrue(connect.isSuccess());
+            assertEquals(List.of("active"), h.events);
+
+            h.channel.read();
+            h.pump();
+            assertEquals(List.of("active", "data:42"), h.events);
+            assertEquals(0, h.allocator.metric().usedHeapMemory());
+        }
+    }
+
+    @Test
+    void retryAndCloseDiscardUnreliableFramesAndRejectLateCallbacks() throws Exception {
+        try (Harness h = new Harness()) {
+            h.channel.pipeline().addFirst(new NetherNetFramingCodec());
+            h.connect();
+            RTCDataChannelObserver old = h.channel.createUnreliableObserver(
+                    (Integer) field(h.channel, "attemptGeneration"));
+            old.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 41}), true));
+            assertTrue(h.allocator.metric().usedHeapMemory() > 0);
+            h.advance(100);
+            assertEquals(0, h.allocator.metric().usedHeapMemory());
+            old.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 42}), true));
+
+            RTCDataChannelObserver current = h.channel.createUnreliableObserver(
+                    (Integer) field(h.channel, "attemptGeneration"));
+            current.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 43}), true));
+            h.observer(new AtomicReference<>(RTCDataChannelState.OPEN)).onStateChange();
+            h.pump();
+            assertEquals(List.of("active", "data:43"), h.events);
+
+            h.channel.close().sync();
+            current.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 44}), true));
+            h.pump();
+            assertEquals(List.of("active", "data:43"), h.events);
+            assertEquals(0, h.allocator.metric().usedHeapMemory());
+        }
     }
 
     private static Object field(NetherNetClientChannel channel, String name) throws Exception {

transport-nethernet/src/test/java/dev/kastle/netty/channel/nethernet/codec/NetherNetFramingCodecTest.java
@@ -1,5 +1,6 @@
 package dev.kastle.netty.channel.nethernet.codec;
 
+import dev.kastle.netty.channel.nethernet.NetherNetUnreliableFrame;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -90,6 +92,47 @@ class NetherNetFramingCodecTest {
         System.arraycopy(part2, 0, expected, part1.length, part2.length);
         System.arraycopy(part3, 0, expected, part1.length + part2.length, part3.length);
         assertArrayEquals(expected, readAll(out));
+    }
+
+    @Test
+    void unreliableMessagesDoNotCompleteReliableAssemblies() {
+        ByteBuf first = framed(2, new byte[]{1});
+        ByteBuf middle = framed(1, new byte[]{2});
+        ByteBuf last = framed(0, new byte[]{3});
+        ByteBuf unreliable = framed(0, new byte[]{42});
+        ByteBuf another = framed(0, new byte[]{43});
+
+        assertFalse(channel.writeInbound(first));
+        assertTrue(channel.writeInbound(new NetherNetUnreliableFrame(unreliable)));
+        assertArrayEquals(new byte[]{42}, readAll(channel.readInbound()));
+        assertEquals(0, unreliable.refCnt());
+        assertFalse(channel.writeInbound(middle));
+        assertTrue(channel.writeInbound(new NetherNetUnreliableFrame(another)));
+        assertArrayEquals(new byte[]{43}, readAll(channel.readInbound()));
+        assertEquals(0, another.refCnt());
+        assertTrue(channel.writeInbound(last));
+        assertArrayEquals(new byte[]{1, 2, 3}, readAll(channel.readInbound()));
+        assertEquals(0, first.refCnt());
+        assertEquals(0, middle.refCnt());
+        assertEquals(0, last.refCnt());
+        assertNull(channel.readInbound());
+    }
+
+    @Test
+    void invalidUnreliableMessagesAreReleasedWithoutDiscardingReliableAssembly() {
+        assertFalse(channel.writeInbound(framed(1, new byte[]{1})));
+        for (ByteBuf invalid : List.of(
+                Unpooled.buffer(1),
+                framed(0, new byte[0]),
+                framed(1, new byte[]{9}),
+                framed(255, new byte[]{9}),
+                framed(0, new byte[NetherNetFramingCodec.MAX_REASSEMBLED_SIZE + 1]))) {
+            assertFalse(channel.writeInbound(new NetherNetUnreliableFrame(invalid)));
+            assertEquals(0, invalid.refCnt());
+        }
+        assertTrue(channel.writeInbound(framed(0, new byte[]{2})));
+        assertArrayEquals(new byte[]{1, 2}, readAll(channel.readInbound()));
+        assertNull(channel.readInbound());
     }
 
     @Test

CloudburstMC/Network

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/NetherNetUnreliableFrame.java
@@ -1,0 +1,23 @@
+package org.cloudburstmc.netty.channel.nethernet;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.DefaultByteBufHolder;
+
+/**
+ * An inbound unreliable message, including its NetherNet header. The framing
+ * codec decodes it independently of any reliable message being reassembled.
+ * Outbound messages still use reliable delivery and must be plain byte buffers.
+ */
+public final class NetherNetUnreliableFrame extends DefaultByteBufHolder {
+    /**
+     * @param data the raw framed message; this holder takes ownership of its reference
+     */
+    public NetherNetUnreliableFrame(ByteBuf data) {
+        super(data);
+    }
+
+    @Override
+    public NetherNetUnreliableFrame replace(ByteBuf content) {
+        return new NetherNetUnreliableFrame(content);
+    }
+}

transport-nethernet/src/main/java/org/cloudburstmc/netty/channel/nethernet/codec/NetherNetFramingCodec.java
@@ -2,6 +2,7 @@ package org.cloudburstmc.netty.channel.nethernet.codec;
 
 import org.cloudburstmc.netty.channel.nethernet.NetherNetChannel;
 import org.cloudburstmc.netty.channel.nethernet.NetherNetConstants;
+import org.cloudburstmc.netty.channel.nethernet.NetherNetUnreliableFrame;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.channel.ChannelDuplexHandler;
@@ -19,7 +20,9 @@ import io.netty.util.internal.logging.InternalLoggerFactory;
  * fragments still to come after this one. A complete single message is
  * header 0 plus payload; a fragmented message counts down to a final
  * fragment with header 0, at which point the accumulated payload is one
- * complete Bedrock batch.
+ * complete Bedrock batch. Unreliable inbound frames must have header 0 and
+ * bypass reliable reassembly. Decoded messages from either channel are byte
+ * buffers; unreliable messages can arrive while a reliable message is incomplete.
  *
  * Inbound reassembly accumulates retained slices in a composite buffer, so
  * reassembly itself copies nothing. Outbound fragments at the maximum
@@ -87,12 +90,13 @@ public class NetherNetFramingCodec extends ChannelDuplexHandler {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (!(msg instanceof ByteBuf)) {
+        boolean unreliable = msg instanceof NetherNetUnreliableFrame;
+        if (!unreliable && !(msg instanceof ByteBuf)) {
             deliveredMessage = true;
             ctx.fireChannelRead(msg);
             return;
         }
-        ByteBuf buf = (ByteBuf) msg;
+        ByteBuf buf = unreliable ? ((NetherNetUnreliableFrame) msg).content() : (ByteBuf) msg;
         receivedFrame = true;
         try {
             if (!buf.isReadable()) {
@@ -100,6 +104,14 @@ public class NetherNetFramingCodec extends ChannelDuplexHandler {
             }
 
             int header = buf.readUnsignedByte();
+            if (unreliable) {
+                // An unreliable message must never complete or reset a reliable assembly.
+                if (header == 0 && buf.isReadable() && buf.readableBytes() <= MAX_REASSEMBLED_SIZE) {
+                    deliveredMessage = true;
+                    ctx.fireChannelRead(buf.readRetainedSlice(buf.readableBytes()));
+                }
+                return;
+            }
             if (buf.readableBytes() > MAX_REASSEMBLED_SIZE) {
                 log.warn("Inbound message exceeds {} bytes, dropping", MAX_REASSEMBLED_SIZE);
                 resetAssembly();

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/NetherNetClientChannelLifecycleTest.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.netty.channel.nethernet;
 
+import org.cloudburstmc.netty.channel.nethernet.codec.NetherNetFramingCodec;
 import org.cloudburstmc.netty.channel.nethernet.config.DefaultNetherClientChannelConfig;
 import org.cloudburstmc.netty.channel.nethernet.config.NetherChannelOption;
 import org.cloudburstmc.netty.channel.nethernet.config.NetherNetAddress;
@@ -292,6 +293,63 @@ class NetherNetClientChannelLifecycleTest {
 
     private static RTCDataChannelBuffer message(int value) {
         return new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{(byte) value}), true);
+    }
+
+    @Test
+    void unreliableMessagesWaitForActivationAndApplicationReads() throws Exception {
+        try (Harness h = new Harness()) {
+            h.channel.pipeline().addFirst(new NetherNetFramingCodec());
+            h.channel.config().setAutoRead(false);
+            ChannelFuture connect = h.connect();
+            RTCDataChannelObserver unreliable = h.channel.createUnreliableObserver(
+                    (Integer) field(h.channel, "attemptGeneration"));
+            unreliable.onStateChange();
+            ByteBuffer callback = ByteBuffer.wrap(new byte[]{99, 0, 42, 99});
+            callback.position(1).limit(3);
+            unreliable.onMessage(new RTCDataChannelBuffer(callback, true));
+            callback.put(2, (byte) 43);
+            h.pump();
+            assertFalse(connect.isDone());
+            assertTrue(h.events.isEmpty());
+
+            h.observer(new AtomicReference<>(RTCDataChannelState.OPEN)).onStateChange();
+            h.pump();
+            assertTrue(connect.isSuccess());
+            assertEquals(List.of("active"), h.events);
+
+            h.channel.read();
+            h.pump();
+            assertEquals(List.of("active", "data:42"), h.events);
+            assertEquals(0, h.allocator.metric().usedHeapMemory());
+        }
+    }
+
+    @Test
+    void retryAndCloseDiscardUnreliableFramesAndRejectLateCallbacks() throws Exception {
+        try (Harness h = new Harness()) {
+            h.channel.pipeline().addFirst(new NetherNetFramingCodec());
+            h.connect();
+            RTCDataChannelObserver old = h.channel.createUnreliableObserver(
+                    (Integer) field(h.channel, "attemptGeneration"));
+            old.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 41}), true));
+            assertTrue(h.allocator.metric().usedHeapMemory() > 0);
+            h.advance(100);
+            assertEquals(0, h.allocator.metric().usedHeapMemory());
+            old.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 42}), true));
+
+            RTCDataChannelObserver current = h.channel.createUnreliableObserver(
+                    (Integer) field(h.channel, "attemptGeneration"));
+            current.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 43}), true));
+            h.observer(new AtomicReference<>(RTCDataChannelState.OPEN)).onStateChange();
+            h.pump();
+            assertEquals(List.of("active", "data:43"), h.events);
+
+            h.channel.close().sync();
+            current.onMessage(new RTCDataChannelBuffer(ByteBuffer.wrap(new byte[]{0, 44}), true));
+            h.pump();
+            assertEquals(List.of("active", "data:43"), h.events);
+            assertEquals(0, h.allocator.metric().usedHeapMemory());
+        }
     }
 
     private static Object field(NetherNetClientChannel channel, String name) throws Exception {

transport-nethernet/src/test/java/org/cloudburstmc/netty/channel/nethernet/codec/NetherNetFramingCodecTest.java
@@ -1,5 +1,6 @@
 package org.cloudburstmc.netty.channel.nethernet.codec;
 
+import org.cloudburstmc.netty.channel.nethernet.NetherNetUnreliableFrame;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -90,6 +92,47 @@ class NetherNetFramingCodecTest {
         System.arraycopy(part2, 0, expected, part1.length, part2.length);
         System.arraycopy(part3, 0, expected, part1.length + part2.length, part3.length);
         assertArrayEquals(expected, readAll(out));
+    }
+
+    @Test
+    void unreliableMessagesDoNotCompleteReliableAssemblies() {
+        ByteBuf first = framed(2, new byte[]{1});
+        ByteBuf middle = framed(1, new byte[]{2});
+        ByteBuf last = framed(0, new byte[]{3});
+        ByteBuf unreliable = framed(0, new byte[]{42});
+        ByteBuf another = framed(0, new byte[]{43});
+
+        assertFalse(channel.writeInbound(first));
+        assertTrue(channel.writeInbound(new NetherNetUnreliableFrame(unreliable)));
+        assertArrayEquals(new byte[]{42}, readAll(channel.readInbound()));
+        assertEquals(0, unreliable.refCnt());
+        assertFalse(channel.writeInbound(middle));
+        assertTrue(channel.writeInbound(new NetherNetUnreliableFrame(another)));
+        assertArrayEquals(new byte[]{43}, readAll(channel.readInbound()));
+        assertEquals(0, another.refCnt());
+        assertTrue(channel.writeInbound(last));
+        assertArrayEquals(new byte[]{1, 2, 3}, readAll(channel.readInbound()));
+        assertEquals(0, first.refCnt());
+        assertEquals(0, middle.refCnt());
+        assertEquals(0, last.refCnt());
+        assertNull(channel.readInbound());
+    }
+
+    @Test
+    void invalidUnreliableMessagesAreReleasedWithoutDiscardingReliableAssembly() {
+        assertFalse(channel.writeInbound(framed(1, new byte[]{1})));
+        for (ByteBuf invalid : List.of(
+                Unpooled.buffer(1),
+                framed(0, new byte[0]),
+                framed(1, new byte[]{9}),
+                framed(255, new byte[]{9}),
+                framed(0, new byte[NetherNetFramingCodec.MAX_REASSEMBLED_SIZE + 1]))) {
+            assertFalse(channel.writeInbound(new NetherNetUnreliableFrame(invalid)));
+            assertEquals(0, invalid.refCnt());
+        }
+        assertTrue(channel.writeInbound(framed(0, new byte[]{2})));
+        assertArrayEquals(new byte[]{1, 2}, readAll(channel.readInbound()));
+        assertNull(channel.readInbound());
     }
 
     @Test